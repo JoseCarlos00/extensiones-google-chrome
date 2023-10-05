babb5486ff8c18ee43dead3f49b6c8566af69595
JoseCarlos00 <101861240+JoseCarlos00@users.noreply.github.com>
2023-10-05 07:22:59 -0600

    refactor: agregando un pedodo a la lista de exlcuidos

--- extencion_alerta/content.js
+++ extencion_alerta/content.js
@@ -114,7 +114,7 @@ function observarCambiosEnNodo(nodoObservado, opciones) {
                     // Aquí puedes manejar los cambios que ocurran en el nodo observado
                     const texto = nodo.children[3].innerText;
                     const expresionRegular = /^356-C-/;
-                    if (expresionRegular.test(texto) && texto !== '356-C-444-69366') {
+                    if (expresionRegular.test(texto) && texto !== '356-C-222-69571') {
                         var notification = new Notification("¡Pedido Nuevo", {
                             icon: "https://bnz06pap003files.storage.live.com/y4m7GAiqY4cGkglOpeEDWUI_01n3gHFX2arSd5eCzwm8pfMmqvd4eAJPOHwtxbyHx42qa4YauXYsb0vqOQIEULk27T8LFS0L1teyIWNBPhnLgpUs4vqRix-KVdaAIRF1t_mnZAiQ8NEcZ8ljECB_SGT0AyYRKbOQ8tLRac52N4MaWIoWfc-M-MGj0wC8osVeLGYZmK7jOCPaRwJ7ou5-uvDb2En_v2CLYcWISHCp2ozGyE?encodeFailures=1&width=48&height=48",
                             body: `Tienes el pedido: ${texto} pendiente`
