4f7dfc5faab34ce4b3ae1be44c808aed0b802f30
JoseCarlos00 <101861240+JoseCarlos00@users.noreply.github.com>
2023-10-07 08:15:15 -0600

    style: finalizacion de la extencion

--- extencion_move_button_save/content.js
+++ extencion_move_button_save/content.js
@@ -1,10 +1,34 @@
+function inicio() {
+
+    // Reducir spaciado
+    document.querySelector("#GridPlaceHolder").style = "min-height: 20px;"
+
+    const style = `
+    <style> 
+        .my-botton-save {
+            position: absolute !important;
+            bottom: -945% !important;
+            left: 32% !important;
+            height: 35px !important;
+
+            & a#NewWaveActionSave {
+                padding-top: 0.35rem !important;
+            }
+        }
+
+        & a#NewWaveActionSave:hover {
+            background-color: #b494bd;
+            height: 35px !important;
+        }
+    </style>`
+
+    setTimeout(() => {
+        document.querySelector("#NewWaveMenu > li.dropdownaction.pull-right.menubutton.menubuttonsave").classList.add('my-botton-save')
+        document.querySelector('head').insertAdjacentHTML('beforeend', style)
+
+    }, 2000)
+
+}
 
 
-const html = `
-<li class="dropdownaction pull-right menubutton menubuttonsave">
-    <a id="NewWaveActionSave" data-resourcekey="BTN_SAVE" data-resourcevalue="Save" href="javascript:;"  style="position: relative; display: block; padding: 10px 15px;">
-        Save
-    </a>
-</li>
-`
-
+window.onload = inicio;
