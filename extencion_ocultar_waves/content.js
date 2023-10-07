48c05e32c03763dfe27fb3a14877f8087686f643
JoseCarlos00 <101861240+JoseCarlos00@users.noreply.github.com>
2023-10-06 22:19:20 -0600

    style: agregando un min-height y display none

--- extencion_ocultar_waves/content.js
+++ extencion_ocultar_waves/content.js
@@ -1,16 +1,18 @@
 const tbody = document.querySelector("#WaveFlowGrid > tbody").childNodes;
 
-const  autoActiva = "1155736963";
+const autoActiva = "1155736963";
 const sinRabasto = "1265266259";
 
 
 tbody.forEach(tr => {
     const DATA_ID = tr.getAttribute('data-id');
 
-    if (DATA_ID !== autoActiva && DATA_ID !== sinRabasto) {
-        tr.style="opacity: 0;"
+    if (DATA_ID !== autoActiva) {
+        tr.style = "opacity: 0; display: none;"
     }
-    
+
 })
+
+document.querySelector("#GridPlaceHolder").style = "min-height: 20px;"
 
 
