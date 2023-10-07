07bcbd9e960c107aa0e77b5e63f551b6b86b7310
JoseCarlos00 <101861240+JoseCarlos00@users.noreply.github.com>
2023-10-07 08:11:05 -0600

    feat: Creacion de carpetas la extencion button save

---
+++ extencion_move_button_save/content.js
@@ -0,0 +1,10 @@
+
+
+const html = `
+<li class="dropdownaction pull-right menubutton menubuttonsave">
+    <a id="NewWaveActionSave" data-resourcekey="BTN_SAVE" data-resourcevalue="Save" href="javascript:;"  style="position: relative; display: block; padding: 10px 15px;">
+        Save
+    </a>
+</li>
+`
+
