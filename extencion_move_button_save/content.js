426100f7f73baf75d459f87ecdae49d8070e11e2
JoseCarlos00 <101861240+JoseCarlos00@users.noreply.github.com>
2023-10-12 14:17:47 -0600

    style: button wave

--- extencion_move_button_save/content.js
+++ extencion_move_button_save/content.js
@@ -26,24 +26,36 @@ function inicio() {
     <style> 
         .my-botton-save {
             position: absolute !important;
-            bottom: -945% !important;
+            bottom: -938% !important;
             left: 32% !important;
             height: 35px !important;
+            border: 1px solid #BCBCBC;
 
             & a#NewWaveActionSave {
                 padding-top: 0.35rem !important;
             }
+
+            & a#NewWaveActionSave:hover {
+                background-color: #a233c1;;
+                height: 35px !important;
+                color: #fff;
+            }
+
         }
 
-        & a#NewWaveActionSave:hover {
-            background-color: #b494bd;
-            height: 35px !important;
+        .nav > li.disabled {
+          background-color: #494e53;
         }
+        .nav > li.disabled > a#NewWaveActionSave {
+            background-color: transparent;
+        }
+        
     </style>`
 
     setTimeout(() => {
-        document.querySelector("#NewWaveMenu > li.dropdownaction.pull-right.menubutton.menubuttonsave").classList.add('my-botton-save')
-        document.querySelector('head').insertAdjacentHTML('beforeend', style)
+        document.querySelector("#NewWaveMenu > li.dropdownaction.pull-right.menubutton.menubuttonsave").classList.add('my-botton-save');
+        document.querySelector("#NewWaveMenu > li.dropdownaction.pull-right.menubutton.menubuttonsave").classList.remove('pull-right');
+        document.querySelector('head').insertAdjacentHTML('beforeend', style);
 
     }, 2000)
 
