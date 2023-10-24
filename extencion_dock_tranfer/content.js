7fd164aabd08ed336d41e5feb49de07619d483ae
JoseCarlos00 <101861240+JoseCarlos00@users.noreply.github.com>
2023-10-24 12:58:06 -0600

    feat: [dock tranfer] terminado

--- extencion_dock_tranfer/content.js
+++ extencion_dock_tranfer/content.js
@@ -1,9 +1,7 @@
-let contenedores = [];
+const head = document.querySelector('head');
 
-function inicio() {
-  const cabecera = document.querySelector('head');
-  const style = document.createElement('style');
-  style.innerHTML = ` 
+const style = `
+<style>
   .insertar:hover { 
       transform: scale(1.1); 
   }
@@ -11,7 +9,7 @@ function inicio() {
       position: absolute;
       top: 56px;
       left: 461px;
-  
+
       display: flex;
       flex-direction: column;
       align-items: center;
@@ -25,21 +23,21 @@ function inicio() {
       margin-bottom: 12px;
       text-align: center;
   }
-  
+
   .notificacion {
       position: absolute;
       top: -31px;
       opacity: -1;
-      transition: all 1s ease-out;
       background-color: #9494d2;
       text-align: center;
+      transition: opacity 0.5s ease;
   }
   .color{
       background-color: #000000;
       color: white;
   }
   .ocultar {
-     opacity: 1;
+    opacity: 1;
   }
   .divFinal > p {
       font-size: 0.8rem;
@@ -51,33 +49,101 @@ function inicio() {
       transition: all 1s ease-out;
   }
   #countRestante {
-     position: absolute;
-     width: 30px;
-  }
-  #dockTranfer {
-    background-color: aqua;
     position: absolute;
-    top: 15px;
-    right: 80px;
+    width: 30px;
+  }
+
+  .container {
+    position: absolute;
+    width: 300px;
+    right: 87px;
+
+    
+    display: flex;
+    flex-direction: column;
+  }
+
+  .container, .textarea {
+    transition: opacity 0.5s ease;
+  }
+
+  .dockTranfer-button {
     height: min-content;
     width: min-content;
-    
-    &:hover {
-      cursor: pontier;
+    cursor: pointer;
+    align-self: flex-end;
+    margin-bottom: 20px;
+  }
+
+  .dockTranfer-button {
+    display: inline-flex;
+    align-items: center;
+    justify-content: center;
+    border: 2px solid #000;
+    border-radius: 4.5rem;
+    background-color: rgba(0, 0, 0, 1);
+    padding: 0rem 1.1rem;
+    text-align: center;
+    color: rgba(255, 255, 255, 1);
+    outline: 0;
+    transition: all  .2s ease;
+    text-decoration: none;
+  }
+
+  .dockTranfer-button:hover {
+    background-color: transparent;
+    color: rgba(0, 0, 0, 1);
+
+    & .icon:hover {
+      color: #000;
     }
   }
+  .icon {
+    color: #fff;
+  }
+</style> 
+`;
+
+head.insertAdjacentHTML('afterend', style);
+
+let contenedores = [];
+
+function inicio() {
+  const html = `
+  <div class="container">
+    <button class="dockTranfer-button"><svg fill="currentColor" class="icon" title="Inserta lista de contenedores" width="32" height="36" viewBox="0 0 49 38" xmlns="http://www.w3.org/2000/svg">
+      <path d="M0.299927 10.6164V9.11638C0.299927 7.87372 1.30727 6.86638 2.54993 6.86638H36.2999V2.36638C36.2999 0.363223 38.728 -0.637558 40.141 0.775348L47.641 8.27535C48.5196 9.15407 48.5196 10.5787 47.641 11.4573L40.141 18.9573C38.7333 20.3647 36.2999 19.3779 36.2999 17.3664V12.8664H2.54993C1.30727 12.8664 0.299927 11.859 0.299927 10.6164ZM46.0499 24.8664H12.2999V20.3664C12.2999 18.3682 9.87536 17.3589 8.4589 18.7753L0.958895 26.2753C0.0802705 27.1541 0.0802705 28.5787 0.958895 29.4573L8.4589 36.9573C9.86777 38.3661 12.2999 37.3761 12.2999 35.3664V30.8664H46.0499C47.2926 30.8664 48.2999 29.859 48.2999 28.6164V27.1164C48.2999 25.8737 47.2926 24.8664 46.0499 24.8664Z"></path>
+      </svg>
+    </button>
+
+    <textarea class="textarea" style="opacity: 0" placeholder="FMA0002459969..." rows="5"></textarea>    
+ </div>
   `;
 
-  cabecera.appendChild(style);
+  document.querySelector('body').insertAdjacentHTML('afterbegin', html);
 
-  const htmIMG = `
-  <button id="dockTranfer"><svg " title="Inserta lista de contenedores" width="49" height="38" viewBox="0 0 49 38" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path d="M0.299927 10.6164V9.11638C0.299927 7.87372 1.30727 6.86638 2.54993 6.86638H36.2999V2.36638C36.2999 0.363223 38.728 -0.637558 40.141 0.775348L47.641 8.27535C48.5196 9.15407 48.5196 10.5787 47.641 11.4573L40.141 18.9573C38.7333 20.3647 36.2999 19.3779 36.2999 17.3664V12.8664H2.54993C1.30727 12.8664 0.299927 11.859 0.299927 10.6164ZM46.0499 24.8664H12.2999V20.3664C12.2999 18.3682 9.87536 17.3589 8.4589 18.7753L0.958895 26.2753C0.0802705 27.1541 0.0802705 28.5787 0.958895 29.4573L8.4589 36.9573C9.86777 38.3661 12.2999 37.3761 12.2999 35.3664V30.8664H46.0499C47.2926 30.8664 48.2999 29.859 48.2999 28.6164V27.1164C48.2999 25.8737 47.2926 24.8664 46.0499 24.8664Z" fill="black"></path>
-</svg></button>
-  `;
-  document.querySelector('body').insertAdjacentHTML('afterbegin', htmIMG);
+  document.querySelector('.dockTranfer-button').addEventListener(
+    'click',
+    () => {
+      document.querySelector('div.container > .textarea').style.opacity = 1;
+      document
+        .querySelector('.dockTranfer-button')
+        .addEventListener('click', insertarContenedores, { once: true });
+    },
+    { once: true }
+  );
+}
 
-  document.querySelector('#dockTranfer').addEventListener('click', content);
+function insertarContenedores() {
+  contenedores = document.querySelector('div.container > .textarea').value.trim().split('\n');
+  console.log(
+    (contenedores = document.querySelector('div.container > .textarea').value.trim().split('\n'))
+  );
+  content();
+
+  setTimeout(() => {
+    document.querySelector('div.container > .textarea').style.opacity = 0;
+  }, 700);
 }
 
 function content() {
@@ -87,47 +153,33 @@ function content() {
   const puerta = document.querySelector('#txtBoxDestLoc');
   const lp = document.querySelector('#txtBoxContainerId');
 
-  const cabecera = document.querySelector('head');
+  const html = `
+  <div class="divFinal">
+    <input class="textoInput" type="text" id="text">
+    <div>
+      <input class="insertar" type="button" value="INSERT">
+    </div>
+    <span class="notificacion" style="width: 180px;">Copiado !</span>
+    <p>LP: <span id="count">1</span> DE <span id="countTotal">9</span></p>
+  </div>
+  `;
 
-  let botonInsertar = document.createElement('div');
-  let cuadroTexto = document.createElement('div');
-
-  let div1 = document.createElement('div');
-  let div2 = document.createElement('div');
-  let span = document.createElement('span');
-
-  //Creacion de Cuadro de texto y voton inser
-  botonInsertar = `<input class='insertar' type="button" value="INSERT" onClick="insertar()">`;
-  cuadroTexto = `<input class='textoInput' type="text" id='text'>`;
-  div1.innerHTML = botonInsertar;
-  div2.innerHTML = cuadroTexto;
-
-  //Agregar al dom el divFinal
-  document.querySelector('#wrapper').appendChild(div2);
-  const divFinal = document.querySelector('#wrapper > div:nth-child(6)');
-
-  divFinal.appendChild(div1);
-  divFinal.appendChild(span);
-
-  divFinal.classList.add('divFinal');
-
-  //Notificaion copiar
-  const spanNotificacion = document.querySelector('#wrapper > div.divFinal > span');
-  spanNotificacion.innerHTML = `Copiado !`;
-  spanNotificacion.classList.add('notificacion');
+  document.querySelector('#wrapper').insertAdjacentHTML('afterend', html);
+  document.querySelector('.divFinal .insertar').addEventListener('click', insertar);
 
   //contadores
-  let contadores = document.createElement('p');
-  contadores.innerHTML = `LP: <span id='count'>0</span> DE <span id='countTotal'>100</span>`;
-  divFinal.appendChild(contadores);
   const count = document.querySelector('#count');
   const countTotal = document.querySelector('#countTotal');
   countTotal.innerHTML = contenedores.length || 0;
 
   //Contenedores Restantes
-  document
-    .querySelector('#wrapper')
-    .insertAdjacentHTML('beforebegin', `<p id='countRestante'>${Number(countTotal.innerHTML)}</p>`);
+  document.querySelector('#wrapper').insertAdjacentHTML(
+    'beforebegin',
+    `
+    <p id='countRestante'>${Number(countTotal.innerHTML)}</p>
+    `
+  );
+
   const countRestante = document.querySelector('#countRestante');
 
   //Botones Nuevos
@@ -136,6 +188,7 @@ function content() {
   const btnEvento = document.querySelector('#wrapper > div:nth-child(8) > input');
 
   let iterador = 0;
+  const spanNotificacion = document.querySelector('div.divFinal > span');
 
   function enviar() {
     if (btnTranfer.disabled === false && puerta.value !== '') {
@@ -146,33 +199,39 @@ function content() {
   }
 
   function insertar() {
-    if (iterador <= contenedores.length - 1) {
+    if (iterador <= contenedores.length - 2) {
       //contadores
       count.innerHTML = iterador + 1;
       inputTexto.value = contenedores[iterador];
-      countRestante.innerHTML = Number(countTotal.innerHTML) + 1 - Number(count.innerHTML);
+      countRestante.innerHTML = Number(countTotal.innerHTML) - Number(count.innerHTML);
       //END
 
       iterador++;
       copiar();
     } else {
+      count.innerHTML = iterador + 1;
       spanNotificacion.style = 'width: 180px;';
       spanNotificacion.innerHTML = 'Ya es el Ultimo! 👌';
       spanNotificacion.classList.add('ocultar');
     }
   }
 
-  function copiar() {
-    inputTexto.focus();
-    document.execCommand('selectALL');
-    document.execCommand('copy');
-    console.log('Copiado al portapapeles');
-    spanNotificacion.classList.add('ocultar');
-    lp.focus();
+  async function copiar() {
+    try {
+      inputTexto.focus();
 
-    setTimeout(() => {
-      spanNotificacion.classList.remove('ocultar');
-    }, 700);
+      const textoCopy = document.querySelector('#text').value;
+      await navigator.clipboard.writeText(textoCopy);
+
+      spanNotificacion.classList.add('ocultar');
+      lp.focus();
+
+      setTimeout(() => {
+        spanNotificacion.classList.remove('ocultar');
+      }, 700);
+    } catch (err) {
+      console.error('Error al copiar al portapapeles:', err);
+    }
   }
 
   document.addEventListener('keydown', function (event) {
@@ -183,7 +242,7 @@ function content() {
   });
 
   document.addEventListener('keydown', function (event) {
-    if (event.key === 'Backspace') {
+    if (event.key === 'Delete') {
       inputTexto.classList.toggle('color');
       insertar();
     }
