70987a2dc7b28525b9717d814d56f70708c18eed
JoseCarlos00 <101861240+JoseCarlos00@users.noreply.github.com>
2023-12-21 09:18:43 -0600

    fix: mercado libre 2
    Condiciones para las lineas de pedidos

--- extencion_MercadoLibre2/content.js
+++ extencion_MercadoLibre2/content.js
@@ -2,36 +2,53 @@
   function inicio() {
     const style = `
     <style>
-      .container.inv-container > img { 
+    .guia, .ultima-imagen{
         object-fit: cover;
         object-position: top;
-        width: 826px;
+        width: 830px;
+      }
+
+      .guia {
         height: 900px;
       }
 
-      .container.inv-container img:first-of-type {
-        height: 400px;
+      .ultima-imagen {
+        height: 500px;
       }
 
-      .container.inv-container .page-break:nth-child(9), .container.inv-container .page-break:nth-child(9) + h3 {
-        display: none;
-      }
     </style>
     `;
     document.querySelector('head').insertAdjacentHTML('beforeend', style);
 
     function intercabiarIMG() {
       return new Promise(resolve => {
-        const images = document.querySelectorAll('.container.inv-container img:first-of-type');
+        const guias = document.querySelectorAll('.container.inv-container img:first-of-type');
 
-        images.forEach(image => {
-          const parent = image.parentNode;
+        guias.forEach(guia => {
+          guia.classList.add('guia');
+
+          // Obtén el nodo padre de la imagen
+          const parent = guia.parentNode;
 
           // Obtén todas las imágenes dentro del mismo nodo padre
           const siblingImages = parent.querySelectorAll('img');
+          const ultimaImagen = siblingImages[siblingImages.length - 1];
+          ultimaImagen.classList.add('ultima-imagen');
+
+          const h3 = parent.querySelectorAll('h3');
+          const tercerH3 = h3[2];
+          const ultimoH3 = h3[h3.length - 1];
+          tercerH3.style.display = 'none';
+          // Obtén todos los elementos con la clase "page-break" del contenedor
+          const pageBreaks = parent.querySelectorAll('.page-break');
+
+          if (pageBreaks.length >= 2) {
+            segundoBreak = pageBreaks[1];
+            segundoBreak.classList.remove('page-break');
+          }
 
           // Intercambia las posiciones de las imágenes
-          parent.insertBefore(siblingImages[1], siblingImages[0]);
+          parent.insertBefore(siblingImages[0], ultimoH3);
         });
 
         // Resuelve la promesa después de intercambiar imágenes
@@ -39,24 +56,40 @@
       });
     }
 
-    function moverH3() {
-      const images = document.querySelectorAll('.container.inv-container img:first-of-type');
+    function resolverLimite() {
+      const referencia = document.querySelectorAll('.container.inv-container .guia');
 
-      images.forEach(image => {
-        const parent = image.parentNode;
+      referencia.forEach(guia => {
+        const parent = guia.parentNode;
 
-        // Obtiene la primera imagen como punto de referencia y el h3 de cada contenedor padre
-        const img = parent.querySelector('img:nth-child(7)');
-        const h3 = parent.querySelector('.container.inv-container img + h3');
+        const tables = parent.querySelectorAll('.container.inv-container table tbody > tr');
+        const ultimaImagen = parent.querySelector('.container.inv-container .ultima-imagen');
+        const lineas = tables.length - 1;
 
-        // Intercambia las posiciones de las imágenes
-        parent.insertBefore(h3, img);
-        h3.style = 'margin-bottom: 80px;';
+        // Si las lineas del pedido paa el limete para mostrar de 6 Mostrar una Hoja Completa
+        if (lineas >= 7 && lineas <= 18) {
+          ultimaImagen.classList.remove('ultima-imagen');
+        }
+
+        if (lineas >= 25 && lineas <= 36) {
+          ultimaImagen.classList.remove('ultima-imagen');
+        }
+
+        if (lineas >= 43 && lineas <= 54) {
+          ultimaImagen.classList.remove('ultima-imagen');
+        }
+
+        if (lineas >= 61 && lineas <= 72) {
+          ultimaImagen.classList.remove('ultima-imagen');
+        }
       });
     }
 
-    // Llama a intercabiarIMG y luego a moverH3 cuando se resuelva la promesa
-    intercabiarIMG().then(() => moverH3());
+    // Llama a intercabiarIMG
+    intercabiarIMG().then(() => {
+      console.log('Imagenes Intercambiadas');
+      resolverLimite();
+    });
   }
 
   window.addEventListener('load', inicio);
