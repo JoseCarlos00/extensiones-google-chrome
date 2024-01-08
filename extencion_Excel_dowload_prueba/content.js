c5db3dfa8dbdf155a6eb6f19158fb0bd7a31135e
JoseCarlos00 <101861240+JoseCarlos00@users.noreply.github.com>
2024-01-08 14:57:50 -0600

    teminete: excel exportaction fail

--- extencion_Excel_dowload_prueba/content.js
+++ extencion_Excel_dowload_prueba/content.js
@@ -1,47 +1,63 @@
 function inicio() {
   console.log('Extension Excel');
 
-  const btnExcelElement =
+  const title =
+    document.querySelector('div div > main.main.container-fluid > h1.text-center').innerText ?? '';
+
+  const btnExelSCALE =
     document.querySelector(
-      '#UpdatePanel > main > div:nth-child(7) > div > div.d-flex.bd-highlight.mb-3 > div.mr-auto.p-2.bd-highlight'
+      '#Panel13 > main > div.row > div > div.d-flex.bd-highlight.mb-3 > div:nth-child(2)'
     ) ?? undefined;
 
-  const btnExcel = `
-  <div class="mr-auto p-2 bd-highlight">
-    <button  id="btnExcel2" class="btn btn-sm text-grey btn-dark-teal mt-3" type="button"><i class="fas fa-file-excel"></i>Excel 2</button>
-  </div>`;
+  const btnExelAR_SCALE =
+    document.querySelector(
+      '#Panel25 > main > div.row > div > div.d-flex.bd-highlight.mb-3 > div:nth-child(2)'
+    ) ?? undefined;
 
-  if (btnExcelElement) {
-    btnExcelElement.classList.remove('mr-auto');
-    btnExcelElement.insertAdjacentHTML('afterend', btnExcel);
+  if (btnExelAR_SCALE) {
+    const btnExcel1 = `
+    <div class="p-2 bd-highlight">
+      <button  id="btnExcel2" class="btn btn-sm text-grey btn-purple mt-3"" type="button"><i class="fas fa-file-excel"></i>Excel</button>
+    </div>`;
 
-    content();
+    btnExelAR_SCALE.insertAdjacentHTML('afterend', btnExcel1);
+    document.querySelector('#btnExcel2').addEventListener('click', e => {
+      const elementID = e.target.getAttribute('id');
+      console.log(elementID);
+      if (elementID == 'btnExcel2') {
+        const tableARSCALE = document.querySelector('#gvtransactionhistoryLI_ctl00') ?? undefined;
+        console.log(tableARSCALE);
+        exportTable(tableARSCALE);
+      }
+    });
   }
 
-  function content() {
-    exportTable();
+  if (btnExelSCALE) {
+    const btnExcel2 = `
+    <div class="p-2 bd-highlight">
+    <button  id="btnExcel3" class="btn btn-sm text-grey btn-purple mt-3"" type="button"><i class="fas fa-file-excel"></i>Excel</button>
+    </div>`;
+    btnExelSCALE.insertAdjacentHTML('afterend', btnExcel2);
+    document.querySelector('#btnExcel3').addEventListener('click', e => {
+      const elementID = e.target.getAttribute('id');
+      console.log(elementID);
+      if (elementID == 'btnExcel3') {
+        const tableScale = document.querySelector('#gvtransactionhistory_ctl00') ?? undefined;
+        console.log(tableScale);
+        exportTable(tableScale);
+      }
+    });
   }
 
-  function exportTable() {
-    // Accede a la tabla por su ID
-    const table = document.querySelector('#gvEnvio_ctl00') ?? undefined;
+  function exportTable(table) {
+    // Usa SheetJS para crear un libro y agregar una hoja con los datos de la tabla
+    const workbook = XLSX.utils.table_to_book(table);
+    console.log('workBook:', workbook);
 
-    if (table) {
-      // Usa SheetJS para crear un libro y agregar una hoja con los datos de la tabla
-      const workbook = XLSX.utils.table_to_book(table);
-      console.log('workBook:', workbook);
+    // Convierte el libro a un archivo blob
+    const blob = XLSX.writeFile(workbook, 'exported_table.xlsx');
 
-      // Convierte el libro a un archivo blob
-      // const blob = XLSX.write(workbook, { bookType: 'xlsx', type: 'blob' });
-      // console.log('blob:', blob);
-
-      // // Crea un objeto URL y descarga el archivo
-      // const url = window.URL.createObjectURL(blob);
-      // chrome.downloads.download({
-      //   url: url,
-      //   filename: 'exported_table.xlsx',
-      // });
-    }
+    console.log('log:', blob);
   }
 }
 
