f5801b600c3742e487a6c9711d3e5a029fa84e5a
JoseCarlos00 <101861240+JoseCarlos00@users.noreply.github.com>
2024-01-09 15:10:23 -0600

    refactor: amz Orion: quitar salto de paguina
    y quitar la imagen clonada y tambier agregar la clase .guia

--- extencion_Amazon_Orion/content.js
+++ extencion_Amazon_Orion/content.js
@@ -9,7 +9,7 @@
   </style>
   `;
 
-  const pageBreak = `<div class="page-break"></div>`;
+  // const pageBreak = `<div class="page-break"></div>`;
   document.querySelector('head').insertAdjacentHTML('beforeend', style);
 
   const container = document.querySelectorAll('.container.inv-container');
@@ -17,17 +17,19 @@
   container.forEach(content => {
     const images = content.querySelectorAll('img');
     const ultimaImagen = images[images.length - 1];
+    const penultimaImagen = images[images.length - 2];
 
     // Clona la última imagen
-    const copiaUltimaImagen = ultimaImagen.cloneNode(true);
+    // const copiaUltimaImagen = ultimaImagen.cloneNode(true);
 
     ultimaImagen.classList.add('guia');
-    copiaUltimaImagen.classList.add('guia');
+    penultimaImagen.classList.add('guia');
+    // copiaUltimaImagen.classList.add('guia');
 
     // Inserta la copia debajo de la última imagen
-    ultimaImagen.parentNode.insertBefore(copiaUltimaImagen, ultimaImagen.nextSibling);
+    // ultimaImagen.parentNode.insertBefore(copiaUltimaImagen, ultimaImagen.nextSibling);
 
-    content.insertAdjacentHTML('beforeend', pageBreak);
+    // content.insertAdjacentHTML('beforeend', pageBreak);
   });
 
   window.addEventListener('load', inicio, { once: true });
