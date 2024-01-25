a41689706002edfdc6c6304e391abc78922bef73
JoseCarlos00 <101861240+JoseCarlos00@users.noreply.github.com>
2024-01-25 16:18:00 -0600

    style: Orion: se agrego el @media print envio y inventory

--- extencion_print/content.js
+++ extencion_print/content.js
@@ -1,9 +1,60 @@
-const style = `
+let ruta = window.location.href;
+ruta = ruta.slice(0, 69);
+
+const styleInventory = `
 <style>
-    div.p-2.bd-highlight button i {
-        padding-right: 4px;
+    @media print {
+      /** Inventory */
+      table#gvInventario_ctl00 {
+        border-color: #000 !important;
+
+        .rgHeader {
+          border-color: #000 !important;
+          font-weight: bold !important;
+        }
+
+        .rgHeader, .rgHeader > a {
+          color: #000 !important;
+        }
+        
+        thead > tr > th, tbody {
+          color: #000 !important;
+          font-weight: bold !important;
+        }
+
+        .rgRow td, .rgAltRow td {
+          border-color: #000 !important;
+        }
+      }
+
+      .d-flex.justify-content-center .btn, .container-print .btn {
+        color: #000 !important;
+        border: 1px solid #000 !important;
+      }
+
+      #divmain-header {
+        .form-control {
+          border: 1px solid #000;
+        }
+    
+        label {
+        color: #000 !important;
+        }
+    
+        .custom-select {
+            border: 1px solid #000;
+        }
+      }
+
+      .table tr th {
+        vertical-align: baseline !important;
+      }
     }
 
+    .grid-container {
+      min-height: calc(100vh - 50px) !important;
+    }
+   
     .container-print {
         gap: 12px;
         padding-left: 15px;
@@ -11,367 +62,50 @@ const style = `
 </style>
 `;
 
-// InsercionesA
-document.querySelector('head').insertAdjacentHTML('beforeend', style);
-
-//  Envio
-function envioPrint() {
-  window.removeEventListener('beforeprint', envioPrint);
-  //Cabecera
-  let arreglo = document.querySelector('#gvEnvio_ctl00 > thead > tr').childNodes;
-
-  for (let i = 1; i < arreglo.length - 1; i++) {
-    let temp = arreglo[i].firstChild;
-    arreglo[i].firstChild.style = 'color: black;';
-  }
-
-  document.querySelector('#gvEnvio_ctl00 > thead > tr > th:nth-child(4)').style =
-    'color: black; background-color: #8CAABD;font-weight: bold;';
-
-  //Cuerpo - Lista
-  document.querySelector('#gvEnvio_ctl00 > tbody').style = 'color: black; font-weight: bold;';
-
-  //Informacion de Envio
-  document.querySelector('#btnInformacion').style = 'color: black; font-weight: bold;';
-  document.querySelector('#btnInformacion > i').style = 'color: black;';
-  document.querySelector('#selTipoEnvio').style =
-    'font-weight: bold; color: black; border: 1px solid black;';
-  document.querySelector('#selCedisSalida').style =
-    'font-weight: bold; color: black; border: 1px solid black;';
-  document.querySelector('#selPrioridad').style =
-    'font-weight: bold; color: black; border: 1px solid black;';
-  document.querySelector(
-    '#UpdatePanel > main > div.main-overview.row > div:nth-child(1) > div'
-  ).style = 'borde: 10x solid';
-
-  //Direcion Envio
-  document.querySelector(
-    '#UpdatePanel > main > div.main-overview.row > div:nth-child(2) > div > button'
-  ).style = 'color: black; font-weight: bold;';
-
-  //Btn Guardar
-  document.querySelector('#btnGuardarEnvio').style = 'color: black !important; border: 1px solid;';
-  document.querySelector('#btnGuardarEnvio > i').style = 'color: black; padding-right: 8px';
-
-  //Btn Enviar Envio
-  document.querySelector('#btnEnviarEnvio').style = 'color: black !important; border: 1px solid;';
-  document.querySelector('#btnEnviarEnvio > i').style = 'padding-right: 8px';
-
-  //Otros Botones
-  document.querySelector('#btnActualizar').style =
-    'color: black !important; border: 1px solid black;';
-  document.querySelector('#btnEliminarPartidas').style =
-    'color: black !important; border: 1px solid black;';
-  document.querySelector('#btnPdf').style = 'color: black !important; border: 1px solid; black;';
-  document.querySelector('#btnExcel').style = 'color: black !important; border: 1px solid black;';
-
-  document.querySelector('#btnComentario').style =
-    'color: black !important; border: 1px solid black;';
-
-  //Btn Ver Envios
-  document.querySelector(
-    '#UpdatePanel > main > div.d-flex.bd-highlight > div:nth-child(1) > a > button'
-  ).style = 'color: black !important; border: 1px solid black;';
-  //Btn Crear Nuevo
-  document.querySelector(
-    '#UpdatePanel > main > div.d-flex.bd-highlight > div:nth-child(2) > a > button'
-  ).style = 'color: black !important; border: 1px solid black;';
-  /*Btn Imprimir */
-  document.querySelector('#printButtonEnvio').style =
-    'color: black !important; border: 1px solid black;';
-  /*Btn Work Unit */
-  document.querySelector('#workUnitButton').style =
-    'color: black !important; border: 1px solid black;';
-
-  //footer
-  document.querySelector('#gvEnvio_ctl00_ctl03_ctl01_PageSizeComboBox > table > tbody').style =
-    'color: black !important; border: 1px solid black;';
-  document.querySelector('#frmEnvio > div:nth-child(59)').style = 'display: none;';
-
-  //Envio Numero, nombre y fecha
-  const numEnvio = document.querySelector('#txtFolioId').textContent;
-  const parametroURL = `
-    <td style=" color: black; padding-left: 20px; border-right: 1px solid transparent;">
-        Generado: <strong>${getParamsURL()[0]}</strong>
-    </td>
-
-    <td style=" color: black; padding-left: 20px; border-right: 1px solid transparent;">
-        <i>${getParamsURL()[1]}</i>
-    </td>
-    `;
-
-  //Insertar el nombre de la persona que hace el envio y fecha
-  document
-    .querySelector(
-      '#divImpresionRepCotizacion > table > tbody > tr:nth-child(1) > td:nth-child(3) > table > tbody > tr:nth-child(1)'
-    )
-    .insertAdjacentHTML('beforeend', parametroURL);
-
-  document.querySelector(
-    '#divImpresionRepCotizacion > table > tbody > tr:nth-child(1) > td:nth-child(3) > table'
-  ).style = 'color: black;';
-
-  document.querySelector(
-    '#divImpresionRepCotizacion > table > tbody > tr:nth-child(1) > td:nth-child(3) > table > tbody > tr:nth-child(1) > td > span'
-  ).textContent = numEnvio; // En la impresion ponemos en el footer el No. Envio
-
-  document.querySelector(
-    '#divImpresionRepCotizacion > table > tbody > tr:nth-child(1) > td:nth-child(3) > table > tbody > tr:nth-child(1) > td'
-  ).style.borderRight = ' 1px solid transparent'; // Border | Entre el evio y la persona generada
-
-  document.querySelector(
-    '#divImpresionRepCotizacion > table > tbody > tr:nth-child(1) > td:nth-child(3) > table > tbody > tr:nth-child(1) > td > span'
-  ).style = 'color: black; borde: none;'; // Ocultamos la tabla de Impresion con datos del envio
-
-  // Contenedor de Footer de Impresion
-  document.querySelector('#UpdatePanel > div.t-container.t-container-static').style.width =
-    'fit-content';
-  document
-    .querySelector('#UpdatePanel > div.t-container.t-container-static > div > div')
-    .classList.remove('t-col');
-
-  // Ocultar Tabla footer
-  document.querySelector('#divImpresionRepCotizacion > table > tbody > tr:nth-child(3)').style =
-    'display: none';
-  document.querySelector('#divImpresionRepCotizacion > table > tbody > tr:nth-child(4)').style =
-    'display: none';
-  document.querySelector('#divImpresionRepCotizacion > table > tbody > tr:nth-child(5)').style =
-    'display: none';
-  document.querySelector('#divImpresionRepCotizacion > table > tbody > tr:nth-child(6)').style =
-    'display: none';
-  document.querySelector('#divImpresionRepCotizacion > table > tbody > tr:nth-child(7)').style =
-    'display: none';
-
-  document.querySelector('body > div > footer').style = 'display: none';
-
-  setTimeout(() => {
-    window.print();
-  }, 500);
-}
-
-function inventoryPrint() {
-  document.querySelector('body > div.grid-container').style.minHeight = 0;
-  // thead --> Encabezados
-  const thead = document.querySelectorAll('#gvInventario_ctl00 > thead > tr > th');
-  thead.forEach(th => {
-    th.style.color = 'black';
-    th.style.fontWeight = 'bold';
-  });
-
-  //tbody --> Cuerpo
-  document.querySelector('#gvInventario_ctl00 > tbody').style = 'color: black; font-weight: bold;';
-
-  // Header
-  styleInventoryBodega();
-
-  // Print
-  setTimeout(() => {
-    window.print();
-  }, 500);
-}
-
-function inventoryNPrint() {
-  document.querySelector('body > div.grid-container').style.minHeight = 0;
-  // thead --> Encabezados
-  const thead = document.querySelectorAll('#gvInventario_ctl00 > thead > tr > th');
-  thead.forEach(th => {
-    th.children[0].style = 'color: black; font-weight: bold;';
-  });
-
-  //tbody --> Cuerpo
-  document.querySelector('#gvInventario_ctl00 > tbody').style = 'color: black; font-weight: bold;';
-
-  //Header
-  styleInventoryBodega();
-
-  // Print
-  setTimeout(() => {
-    window.print();
-  }, 500);
-}
-
-/** Work Unit */
-function workUnitInsert() {
-  const body = document.querySelector('body');
-
-  let work_unit = prompt() ?? '';
-  if (work_unit) {
-    work_unit = work_unit.trim();
-
-    body.insertAdjacentHTML(
-      'afterbegin',
-      `<div style="position: absolute; right: 100px; top: 286px; font-size: 34px;">
-              Work Unit: <spam style='font-weight: bold; font-size: 38px'>  ${work_unit} </spam>
-        </div>
-        `
-    );
-  }
-}
-// END
-
-/** Lista de Envios */
-function listEnvios() {
-  const filaPedidos = document.querySelectorAll('#gvEnvioListas_ctl00 > tbody tr');
-
-  filaPedidos.forEach(tr => {
-    const GENERADO_POR = tr.children[9].innerText;
-    const FECHA_ENVIO = tr.children[5].innerText;
-    let href = tr.children[2].children[0].getAttribute('href');
-
-    tr.children[2].children[0].setAttribute('target', '_blank');
-    href += `&userEnvio=${GENERADO_POR}&fechaEnvio=${FECHA_ENVIO}`;
-
-    tr.children[2].children[0].setAttribute('href', href);
-  });
-
-  const btnBuscar = document.querySelector('#btnBuscar') ?? undefined;
-  btnBuscar.addEventListener('click', () => {
-    setTimeout(listEnvios, 1500);
-  });
-}
-//end
-
-/** Obtener Datos de la URL */
-/**
- *  getParamsURL( )[ 0 ] Acceder a un parametro en especifico
- * @returns Array con los parametros de la URL
- */
-function getParamsURL() {
-  const urlString = window.location.href; // Obtener la URL actual
-  const url = new URL(urlString); // Crear un objeto URL
-  const parametros = url.searchParams; // Obtener los parámetros de la URL
-  const userEnvio = parametros.get('userEnvio') ?? '';
-  const fechaEnvio = parametros.get('fechaEnvio') ?? '';
-
-  let parametro = [];
-  parametro.push(userEnvio);
-  parametro.push(fechaEnvio);
-
-  return parametro;
-}
-// END
-
-/** Envio item */
-function envioItem() {
-  // Boton imprimir
-  const button = `
-  <div class="p-2 bd-highlight">
-      <button id="printButtonEnvio" class="btn btn-secondary btn-sm btn-dark-teal" type="button"><i class="fas fa-print"></i>Imprimir</button>
-  </div>
-
-  <div class="p-2 bd-highlight">
-      <button id="workUnitButton" class="btn btn-secondary btn-sm btn-dark-teal" type="button"><i class="fas fa-file-word"></i>Work Unit</button>
-  </div>
-  `;
-  const elementoInsert = document.querySelector(
-    '#UpdatePanel > main > div.d-flex.bd-highlight > div:nth-child(2)'
-  );
-  elementoInsert.insertAdjacentHTML('afterend', button);
-
-  //Evento
-  document.querySelector('#printButtonEnvio').addEventListener('click', envioPrint);
-  document.querySelector('#workUnitButton').addEventListener('click', workUnitInsert);
-  window.addEventListener('beforeprint', envioPrint, { once: true });
-
-  // Titulo envio  => "Envio # 12679"
-  document.querySelector(
-    '#UpdatePanel > main > div.d-flex.bd-highlight > div.flex-grow-1.bd-highlight'
-  ).style = 'padding-right: calc(88.22px + 0.5rem*2 + 0.5rem + 110.18px);';
-}
-// END
-
-/** Inventario Bodega */
-function inventarioBodega() {
-  const elementoInsert = document.querySelector(
-    '#frmConsultaMiodani > main > div.row > div > div > div.card-table > div.form-inline'
-  );
-  elementoInsert.classList.add('container-print');
-  elementoInsert.children[0].classList.remove('col');
-
-  // Boton imprimir
-  const button = `
+// Boton imprimir
+const buttonPrint = `
         <div >
             <button id="printButtonInventory" type="button" class="btn btn-sm btn-purple"><i class="fas fa-print"></i>Imprimir</button>
         </div>
         `;
-  elementoInsert.insertAdjacentHTML('beforeend', button);
+
+function insertButtonImprimir(elementoInsert) {
+  elementoInsert.classList.add('container-print');
+  elementoInsert.children[0].classList.remove('col');
+
+  elementoInsert.insertAdjacentHTML('beforeend', buttonPrint);
 
   //Evento
-  document.querySelector('#printButtonInventory').addEventListener('click', inventoryPrint);
-  window.addEventListener('beforeprint', inventoryPrint, { once: true });
+  document.querySelector('#printButtonInventory').addEventListener('click', () => window.print());
+}
+
+/** Inventario Bodega */
+function inventarioBodega() {
+  // Inserciones
+  document.querySelector('head').insertAdjacentHTML('beforeend', styleInventory);
+
+  const elementoInsert = document.querySelector(
+    '#frmConsultaMiodani > main > div.row > div > div > div.card-table > div.form-inline'
+  );
+  insertButtonImprimir(elementoInsert);
 }
 // END
 
 /** Inventario Bodega Separado N */
 function inventarioBodegaN() {
+  // Inserciones
+  document.querySelector('head').insertAdjacentHTML('beforeend', styleInventory);
+
   const elementoInsert = document.querySelector(
     '#frmInventarioSeparado > main > div.row > div > div > div.card-table > div.form-inline'
   );
-
-  elementoInsert.classList.add('container-print');
-  elementoInsert.children[0].classList.remove('col');
-
-  // Boton imprimir
-  const button = `
-            <div >
-                <button id="printButtonInventory" type="button" class="btn btn-sm btn-purple"><i class="fas fa-print"></i>Imprimir</button>
-            </div>
-            `;
-  elementoInsert.insertAdjacentHTML('beforeend', button);
-
-  //Evento
-  document.querySelector('#printButtonInventory').addEventListener('click', inventoryNPrint);
-  window.addEventListener('beforeprint', inventoryNPrint, { once: true });
+  insertButtonImprimir(elementoInsert);
 }
 // END
 
-function styleInventoryBodega() {
-  const style = `
-  <style>
-    .overview-card {
-      border: 1px solid #000;
-
-      & button {
-          color: #000;
-      }
-    }
-
-    .form-control {
-      border: 1px solid #000;
-    }
-
-    label {
-    color: #000 !important;
-    }
-
-    .custom-select {
-        border: 1px solid #000;
-    }
-
-    .btn-block, .btn {
-        color: #000 !important;
-        border: 1px solid #000;
-    }
-</style>
-  `;
-
-  document.querySelector('head').insertAdjacentHTML('beforeend', style);
-}
-
 // Condicionales
-let ruta = window.location.href;
-ruta = ruta.slice(0, 69);
-
-if (ruta === 'http://fmorion.dnsalias.com/orion/paginas/Envios/Envio.aspx?EnvioNum=') {
-  envioItem();
-}
-
 if (ruta === 'http://fmorion.dnsalias.com/orion/paginas/Medidas/InventarioBodega.as') {
   inventarioBodega();
-}
-
-if (ruta === 'http://fmorion.dnsalias.com/orion/paginas/Envios/EnviosListas.aspx') {
-  setTimeout(listEnvios, 1500);
 }
 
 if (ruta === 'http://fmorion.dnsalias.com/orion/paginas/Medidas/InventarioSeparadoN') {
--- extencion_print/popup.js
+++ extencion_print/popup.js
@@ -1,3 +1,223 @@
-const btnPintar = document.querySelector('#btn-pintar');
-btnPintar.addEventListener('click', pintar1);
+//  Envio
+const styleEnvio = `
+<style>
+  /** Envio */
+  @media print {
 
+    table#gvEnvio_ctl00 {
+      border-color: #000 !important;
+
+      .rgHeader {
+        border-color: #000 !important;
+        font-weight: bold !important;
+      }
+    
+      .rgHeader, .rgHeader > a {
+        color: #000 !important;
+      }
+
+      tbody {
+        font-weight: bold !important;
+      }
+
+      .rgRow td, .rgAltRow td {
+        border-color: #000 !important;
+      }
+    }
+
+     .main.container-fluid  .d-flex.bd-highlight .btn, #btnGuardarEnvio, #btnComentario {
+        color: #000 !important;
+        border: 1px solid #000 !important;
+     }
+
+     .main-overview {
+        .overview-card .btn-link {
+          font-weight: bold;
+          color: #000 !important;
+        }
+
+        .custom-select:disabled {
+          color: #000 !important;
+          font-weight: bold !important;
+          border: 1px solid #000 !important;
+        }
+     }
+
+     #btnGuardarEnvio > i, #btnEnviarEnvio > i {
+      padding-right: 8px !important;
+     }
+    
+     #frmEnvio > div:nth-child(59), footer.footer, 
+     #divImpresionRepCotizacion > table > tbody > tr:is(:nth-child(3), :nth-child(4), :nth-child(5), :nth-child(6), :nth-child(7)) {
+      display: none;
+     }
+
+     .table tr th {
+      vertical-align: baseline !important;
+    }
+  }
+
+  #UpdatePanel > div.t-container.t-container-static table tr td:nth-child(3) table {
+    border: none !important;
+  }
+
+  #UpdatePanel > div.t-container.t-container-static table tr td:nth-child(3) table tbody {
+    display: flex;
+    width: max-content;
+    border-top: transparent;
+    border-left: transparent;
+    border-right: transparent;
+    
+    span {
+      color: #000 !important;
+    }
+
+    tr:nth-child(1) {
+      td {
+        background-color: transparent !important;
+        padding-left: 4px !important;
+        padding-right: 4px !important;
+      }
+
+    }
+
+    tr:nth-child(2) {
+      span {
+        padding-left: 8px !important;
+        padding-right: 4px !important;
+      }
+    }
+  }
+
+  div.p-2.bd-highlight button i {
+    padding-right: 4px;
+  }
+
+
+</style>
+`;
+
+/** Work Unit */
+function workUnitInsert() {
+  const body = document.querySelector('body');
+  const WORK_UNIT = document.querySelector('#workUnit') ?? undefined;
+
+  let workUnit = prompt() ?? '';
+
+  if (WORK_UNIT) {
+    WORK_UNIT.innerText = workUnit;
+    return;
+  }
+
+  if (workUnit) {
+    workUnit = workUnit.trim();
+
+    body.insertAdjacentHTML(
+      'afterbegin',
+      `<div style="position: absolute; right: 100px; top: 286px; font-size: 34px;">
+              Work Unit: <spam id="workUnit" style='font-weight: bold; font-size: 38px'>  ${workUnit} </spam>
+        </div>
+        `
+    );
+  }
+}
+// END
+
+/** Lista de Envios */
+function listEnvios() {
+  const filaPedidos = document.querySelectorAll('#gvEnvioListas_ctl00 > tbody tr');
+
+  filaPedidos.forEach(tr => {
+    const GENERADO_POR = tr.children[9].innerText;
+    const FECHA_ENVIO = tr.children[5].innerText;
+    let href = tr.children[2].children[0].getAttribute('href');
+
+    tr.children[2].children[0].setAttribute('target', '_blank');
+    href += `&userEnvio=${GENERADO_POR}&fechaEnvio=${FECHA_ENVIO}`;
+
+    tr.children[2].children[0].setAttribute('href', href);
+  });
+
+  const btnBuscar = document.querySelector('#btnBuscar') ?? undefined;
+  btnBuscar.addEventListener('click', () => {
+    setTimeout(listEnvios, 1500);
+  });
+}
+//end
+
+/** Obtener Datos de la URL */
+/**
+ *  getParamsURL( )[ 0 ] Acceder a un parametro en especifico
+ * @returns Array con los parametros de la URL
+ */
+function getParamsURL() {
+  const urlString = window.location.href; // Obtener la URL actual
+  const url = new URL(urlString); // Crear un objeto URL
+  const parametros = url.searchParams; // Obtener los parámetros de la URL
+  const userEnvio = parametros.get('userEnvio') ?? '';
+  const fechaEnvio = parametros.get('fechaEnvio') ?? '';
+
+  let parametro = [];
+  parametro.push(userEnvio);
+  parametro.push(fechaEnvio);
+
+  return parametro;
+}
+// END
+
+/** Envio item */
+function envioItem() {
+  // Insertar estilos
+  document.querySelector('head').insertAdjacentHTML('beforeend', styleEnvio);
+  // Boton imprimir
+  const button = `
+  <div class="p-2 bd-highlight">
+      <button id="printButtonEnvio" class="btn btn-secondary btn-sm btn-dark-teal" type="button"><i class="fas fa-print"></i>Imprimir</button>
+  </div>
+
+  <div class="p-2 bd-highlight">
+      <button id="workUnitButton" class="btn btn-secondary btn-sm btn-dark-teal" type="button"><i class="fas fa-file-word"></i>Work Unit</button>
+  </div>
+  `;
+  const elementoInsert = document.querySelector(
+    '#UpdatePanel > main > div.d-flex.bd-highlight > div:nth-child(2)'
+  );
+
+  elementoInsert.insertAdjacentHTML('afterend', button);
+
+  //Evento
+  document.querySelector('#printButtonEnvio').addEventListener('click', () => window.print());
+  document.querySelector('#workUnitButton').addEventListener('click', workUnitInsert);
+
+  // Titulo envio  => "Envio # 12679"
+  document.querySelector(
+    '#UpdatePanel > main > div.d-flex.bd-highlight > div.flex-grow-1.bd-highlight'
+  ).style = 'padding-right: calc(88.22px + 0.5rem*2 + 0.5rem + 110.18px);';
+
+  // En la impresion ponemos en el footer el No. Envio
+  //Envio Numero, nombre y fecha
+  const numEnvio = document.querySelector('#txtFolioId').textContent;
+  document.querySelector(
+    '#divImpresionRepCotizacion > table > tbody > tr:nth-child(1) > td:nth-child(3) > table > tbody > tr:nth-child(1) > td > span'
+  ).textContent = numEnvio;
+
+  const envioInfo = document.querySelector('#lblnumenvio');
+  envioInfo.style = '';
+
+  envioInfo.innerHTML = `Generado: <strong>${getParamsURL()[0]}</strong> ${getParamsURL()[1]}`;
+
+  // Contenedor  de Impresion
+  document
+    .querySelector('#UpdatePanel > div.t-container.t-container-static > div > div')
+    .classList.remove('t-col');
+}
+// END
+
+// Condicionales
+if (ruta === 'http://fmorion.dnsalias.com/orion/paginas/Envios/EnviosListas.aspx') {
+  setTimeout(listEnvios, 1500);
+}
+
+if (ruta === 'http://fmorion.dnsalias.com/orion/paginas/Envios/Envio.aspx?EnvioNum=') {
+  envioItem();
+}
