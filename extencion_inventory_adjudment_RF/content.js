100401249beea6bdc5efd6afbaab1551138ff0b8
JoseCarlos00 <101861240+JoseCarlos00@users.noreply.github.com>
2024-01-31 16:31:06 -0600

    feat: adjusment: boton de pause y cancel

--- extencion_inventory_adjudment_RF/content.js
+++ extencion_inventory_adjudment_RF/content.js
@@ -1,4 +1,6 @@
 function inicio() {
+  let pause = true;
+
   function getAdjType(iIndex) {
     if (iIndex == 0) return 'Ajuste Negativo';
     if (iIndex == 1) return 'Ajuste Negativo LPN';
@@ -12,6 +14,7 @@ function inicio() {
   }
 
   let stAdj = getAdjType(form1.adjType.selectedIndex) ?? undefined;
+
   if (stAdj == 'Ajuste Positivo') {
     /**Estilos */
     document.querySelector('head').insertAdjacentHTML(
@@ -27,7 +30,11 @@ function inicio() {
 
       animation: entradaElemento 0.5s ease-in-out;
 
-      & button {
+      div {
+        margin: 0 auto;
+      }
+
+      & button:nth-child(2) {
         align-self: center;
         width: 90px;
         cursor: pointer;
@@ -37,11 +44,11 @@ function inicio() {
         background-color: #0dd406;
       }
 
-      & button:hover {
+      & button:nth-child(2):hover {
         background: #0dd406d6;
       }
       
-      & button:active {
+      & button:nth-child(2):active {
         background: #0dd406bf;
       }
     }
@@ -63,6 +70,11 @@ function inicio() {
       from { opacity: 0; transform: translateY(-20px); }
       to { opacity: 1; transform: translateY(0); }
     }
+
+    /** Otros estilos */
+    #table1 > tbody > tr:nth-child(9) > td {
+      padding: 12px 0 0 19px;
+    }
     </style>
     `
     );
@@ -73,9 +85,13 @@ function inicio() {
     const formulario = `
       <form id="registroForm">
         <label for="ubicaciones">Item, Qty y Ubicacion:</label>
-        <textarea id="ubicaciones" name="ubicaciones" rows="4" cols="50" required placeholder="8264-10104-10618   1pz    1-25-02-AA-01    FMA0002376952(Opcional)"></textarea>
+        <textarea id="ubicaciones" name="ubicaciones" rows="4" cols="50" required placeholder="Item\t\t\tPiezas\tUbicacion\tLP(Opcional)\n8264-10104-10618\t1pz\t1-25-02-AA-01\tFMA0002376952"></textarea>
         
-        <button id="registraUbicaciones" type="button">Registrar</button>
+        <div>
+          <button id="pausar" type="button">Pausar</button>
+          <button id="registraUbicaciones" type="button">Registrar</button>
+          <button id="cancelar" type="button">Cancel</button>
+        </div>
       </form>`;
 
     body.insertAdjacentHTML('afterbegin', formulario);
@@ -90,7 +106,25 @@ function inicio() {
       `;
 
     body.insertAdjacentHTML('beforeend', contadores);
+
+    // Verificar si esta la variable PAUSE almacenado al cargar la página
+    if (chrome.storage) {
+      chrome.storage.local.get('pauseChrome', result => {
+        const pauseChrome = result.pauseChrome;
+
+        if (pauseChrome) {
+          pause = pauseChrome;
+          console.log('Pause Get:', pauseChrome);
+        } else {
+          console.log('No se encontro PAUSE guardado.');
+        }
+      });
+    } else {
+      console.error('chrome.storage no está disponible.');
+    }
+
     content();
+    document.getElementById('cancelar').addEventListener('click', alertaCanceladora);
   }
 
   function content() {
@@ -162,16 +196,21 @@ function inicio() {
       setTimeout(function () {
         console.log('insertarDatos completado exitosamente');
         if (chrome.storage) {
-          // Tu código que utiliza chrome.storage aquí
-          // Guardar los datos en el almacenamiento local
-          chrome.storage.local.set({ datosGuardados: datos }, function () {
+          chrome.storage.local.set({ datosAdjustment: datos }, () => {
             console.log('Datos guardados en el almacenamiento local.');
+          });
+
+          chrome.storage.local.set({ pauseChrome: pause }, () => {
+            console.log('Pause Set1:', pause);
           });
         } else {
           console.error('chrome.storage no está disponible.');
         }
 
-        form1.submit();
+        if (pause) {
+          console.log('Submit');
+          form1.submit();
+        }
       }, 1500);
     }
 
@@ -183,21 +222,54 @@ function inicio() {
     // Verificar si hay datos almacenados al cargar la página
     if (chrome.storage) {
       // Tu código que utiliza chrome.storage aquí
-      chrome.storage.local.get('datosGuardados', function (result) {
-        const datosGuardados = result.datosGuardados;
-        const datosGuardadosNum = Object.keys(datosGuardados).length;
+      chrome.storage.local.get('datosAdjustment', result => {
+        const datosAdjustment = result.datosAdjustment;
 
-        if (datosGuardados) {
-          console.log('Se encontraron datos guardados:', datosGuardadosNum, datosGuardados);
+        if (datosAdjustment) {
+          const datosAdjustmentNum = Object.keys(datosAdjustment).length;
+          console.log('Se encontraron datos guardados:', datosAdjustmentNum, datosAdjustment);
 
-          contador(datosGuardadosNum);
-          insertarDatos(datosGuardados);
+          contador(datosAdjustmentNum);
+          insertarDatos(datosAdjustment);
         } else {
           console.log('No se encontraron datos guardados.');
         }
       });
     } else {
       console.error('chrome.storage no está disponible.');
+    }
+
+    document.getElementById('pausar').addEventListener('click', () => {
+      pause = false;
+
+      if (chrome.storage) {
+        chrome.storage.local.set({ pauseChrome: pause }, () => {
+          console.log('Pause Set2:', pause);
+        });
+      } else {
+        console.error('chrome.storage no está disponible.');
+      }
+    });
+  }
+
+  function alertaCanceladora() {
+    const tiempoEspera = 250;
+
+    // Mostrar una alerta que permita al usuario cancelar la ejecución de la función
+    const confirmacion = confirm('¿Quieres cancelar?');
+    if (confirmacion) {
+      if (chrome.storage) {
+        chrome.storage.local.remove('datosAdjustment', function () {
+          console.log('datos borrados correctamente.');
+          setTimeout(() => {
+            window.location.reload();
+          }, tiempoEspera);
+        });
+      } else {
+        console.error('chrome.storage no está disponible.');
+      }
+    } else {
+      console.log('La función no se ha ejecutado.');
     }
   }
 }
--- extencion_inventory_adjudment_RF/popup.js
+++ extencion_inventory_adjudment_RF/popup.js
@@ -1,4 +1,5 @@
 function inicio() {
+  let pause = true;
   function getAdjType(iIndex) {
     if (iIndex == 0) return 'Ajuste Negativo';
     if (iIndex == 1) return 'Ajuste Negativo LPN';
@@ -28,7 +29,11 @@ function inicio() {
 
       animation: entradaElemento 0.5s ease-in-out;
 
-      & button {
+      div {
+        margin: 0 auto;
+      }
+
+      & button:nth-child(2) {
         align-self: center;
         width: 90px;
         cursor: pointer;
@@ -38,11 +43,11 @@ function inicio() {
         background-color: #0dd406;
       }
 
-      & button:hover {
+      & button:nth-child(2):hover {
         background: #0dd406d6;
       }
       
-      & button:active {
+      & button:nth-child(2):active {
         background: #0dd406bf;
       }
     }
@@ -64,6 +69,11 @@ function inicio() {
       from { opacity: 0; transform: translateY(-20px); }
       to { opacity: 1; transform: translateY(0); }
     }
+
+    /** Otros estilos */
+    #table1 > tbody > tr:nth-child(10) > td {
+      padding: 12px 0 0 46px;
+    }
     </style>
     `
     );
@@ -74,9 +84,13 @@ function inicio() {
     const formulario = `
       <form id="registroForm">
         <label for="ubicaciones">Item, Qty, From Ubicacion, To Ubicacion, LP:</label>
-        <textarea id="ubicaciones" name="ubicaciones" rows="4" cols="50" required placeholder="8264-10104-10618   1pz    1-25-02-AA-01(From)   1-25-02-AA-01(To)   FMA0002376952"></textarea>
+        <textarea id="ubicaciones" name="ubicaciones" rows="4" cols="50" required placeholder="Item\t\t\tPiezas\tUbi. origen\tUbi. destino\tLP origen\n8264-10104-10618\t1pz\t1-25-02-AA-01\t1-25-02-AA-01\tFMA0002376952"></textarea>
         
-        <button id="registraUbicaciones" type="button">Registrar</button>
+        <div>
+          <button id="pausar" type="button">Pausar</button>
+          <button id="registraUbicaciones" type="button">Registrar</button>
+          <button id="cancelar" type="button">Cancel</button>
+        </div>
       </form>`;
 
     body.insertAdjacentHTML('afterbegin', formulario);
@@ -91,120 +105,178 @@ function inicio() {
       `;
 
     body.insertAdjacentHTML('beforeend', contadores);
-    content();
-  }
 
-  function content() {
-    // Objeto para almacenar los datos
-
-    document.querySelector('#registraUbicaciones').addEventListener('click', registrarDatos);
-
-    function registrarDatos() {
-      const datos = {};
-      const ubicaciones = document.getElementById('ubicaciones').value;
-
-      if (ubicaciones) {
-        // Dividir el texto en líneas
-        const lineas = ubicaciones.split('\n');
-
-        // Contador para asignar claves numéricas únicas
-        let contador = 0;
-
-        // Procesar cada línea
-        lineas.forEach(linea => {
-          const match = linea.match(/^(\d+-\d+-\d+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)/);
-
-          if (match) {
-            const item = match[1] ?? null;
-            const qty = Number(match[2]) ?? null;
-            const fromLoc = match[3] ?? null;
-            const toLoc = match[4] ?? null;
-            const LP = match[5] ?? null;
-            // console.log('lp:', LP);
-            // console.log('match', match);
-
-            if (!item || !qty) return;
-
-            // Agregar datos al objeto usando el contador como clave
-            datos[contador++] = { item, qty, fromLoc, toLoc, LP };
-          }
-        });
-
-        // Limpiar el campo de texto
-        document.getElementById('ubicaciones').value = '';
-
-        // Insertar datos
-        console.log(datos);
-        insertarDatos(datos);
-      }
-    } // End Ubicaciones
-
-    function insertarDatos(datos) {
-      // Obtener las claves (números de artículo) del objeto datos
-      const filas = Object.keys(datos);
-      contador(filas.length);
-      // Verificar si hay datos para procesar
-      if (filas.length === 0) {
-        console.log('No hay datos para insertar.');
-        return;
-      }
-
-      // console.log('datos:', datos);
-
-      // Obtener la primera fila del objeto datos
-      const primerFila = datos[filas[0]];
-      // console.log('primerFila:', primerFila);
-      // Asignar valores al formulario
-      form1.item.value = primerFila.item;
-      form1.company.value = 'FM';
-      form1.quantity.value = primerFila.qty;
-      form1.QTYUM.value = 'PZ (1,00)';
-      form1.RFLOGISTICSUNIT.value = primerFila.LP;
-      form1.fromLoc.value = primerFila.fromLoc;
-      form1.toLoc.value = primerFila.toLoc;
-
-      // Simular una operación asincrónica, por ejemplo, un temporizador
-      delete datos[filas[0]];
-      setTimeout(function () {
-        console.log('insertarDatos completado exitosamente');
-        if (chrome.storage) {
-          // Tu código que utiliza chrome.storage aquí
-          // Guardar los datos en el almacenamiento local
-          chrome.storage.local.set({ datosGuardados: datos }, function () {
-            console.log('Datos guardados en el almacenamiento local.');
-          });
-        } else {
-          console.error('chrome.storage no está disponible.');
-        }
-
-        form1.submit();
-      }, 1500);
-    }
-
-    function contador(value) {
-      const countRestante = document.querySelector('#countRestante');
-      countRestante.innerHTML = `${value}`;
-    }
-
-    // Verificar si hay datos almacenados al cargar la página
+    // Verificar si esta la variable PAUSE almacenado al cargar la página
     if (chrome.storage) {
-      // Tu código que utiliza chrome.storage aquí
-      chrome.storage.local.get('datosGuardados', function (result) {
-        const datosGuardados = result.datosGuardados;
-        const datosGuardadosNum = Object.keys(datosGuardados).length;
+      chrome.storage.local.get('pauseChrome', result => {
+        const pauseChrome = result.pauseChrome;
 
-        if (datosGuardados) {
-          console.log('Se encontraron datos guardados:', datosGuardadosNum, datosGuardados);
-
-          contador(datosGuardadosNum);
-          insertarDatos(datosGuardados);
+        if (pauseChrome) {
+          pause = pauseChrome;
+          console.log('Pause Get:', pauseChrome);
         } else {
-          console.log('No se encontraron datos guardados.');
+          console.log('No se encontro PAUSE guardado.');
         }
       });
     } else {
       console.error('chrome.storage no está disponible.');
     }
+
+    content();
+    document.getElementById('cancelar').addEventListener('click', alertaCanceladora);
+  }
+}
+
+function content() {
+  // Objeto para almacenar los datos
+
+  document.querySelector('#registraUbicaciones').addEventListener('click', registrarDatos);
+
+  function registrarDatos() {
+    const datos = {};
+    const ubicaciones = document.getElementById('ubicaciones').value;
+
+    if (ubicaciones) {
+      // Dividir el texto en líneas
+      const lineas = ubicaciones.split('\n');
+
+      // Contador para asignar claves numéricas únicas
+      let contador = 0;
+
+      // Procesar cada línea
+      lineas.forEach(linea => {
+        const match = linea.match(/^(\d+-\d+-\d+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)/);
+
+        if (match) {
+          const item = match[1] ?? null;
+          const qty = Number(match[2]) ?? null;
+          const fromLoc = match[3] ?? null;
+          const toLoc = match[4] ?? null;
+          const LP = match[5] ?? null;
+          // console.log('lp:', LP);
+          // console.log('match', match);
+
+          if (!item || !qty) return;
+
+          // Agregar datos al objeto usando el contador como clave
+          datos[contador++] = { item, qty, fromLoc, toLoc, LP };
+        }
+      });
+
+      // Limpiar el campo de texto
+      document.getElementById('ubicaciones').value = '';
+
+      // Insertar datos
+      console.log(datos);
+      insertarDatos(datos);
+    }
+  } // End Ubicaciones
+
+  function insertarDatos(datos) {
+    // Obtener las claves (números de artículo) del objeto datos
+    const filas = Object.keys(datos);
+    contador(filas.length);
+    // Verificar si hay datos para procesar
+    if (filas.length === 0) {
+      console.log('No hay datos para insertar.');
+      return;
+    }
+
+    // console.log('datos:', datos);
+
+    // Obtener la primera fila del objeto datos
+    const primerFila = datos[filas[0]];
+    // console.log('primerFila:', primerFila);
+    // Asignar valores al formulario
+    form1.item.value = primerFila.item;
+    form1.company.value = 'FM';
+    form1.quantity.value = primerFila.qty;
+    form1.QTYUM.value = 'PZ (1,00)';
+    form1.RFLOGISTICSUNIT.value = primerFila.LP;
+    form1.fromLoc.value = primerFila.fromLoc;
+    form1.toLoc.value = primerFila.toLoc;
+
+    // Simular una operación asincrónica, por ejemplo, un temporizador
+    delete datos[filas[0]];
+    setTimeout(function () {
+      console.log('insertarDatos completado exitosamente');
+      if (chrome.storage) {
+        // Tu código que utiliza chrome.storage aquí
+        // Guardar los datos en el almacenamiento local
+        chrome.storage.local.set({ datosTranferManual: datos }, function () {
+          console.log('Datos guardados en el almacenamiento local.');
+        });
+
+        chrome.storage.local.set({ pauseChrome: pause }, () => {
+          console.log('Pause Set1:', pause);
+        });
+      } else {
+        console.error('chrome.storage no está disponible.');
+      }
+
+      if (pause) {
+        console.log('Submit');
+        form1.submit();
+      }
+    }, 1500);
+  }
+
+  function contador(value) {
+    const countRestante = document.querySelector('#countRestante');
+    countRestante.innerHTML = `${value}`;
+  }
+
+  // Verificar si hay datos almacenados al cargar la página
+  if (chrome.storage) {
+    // Tu código que utiliza chrome.storage aquí
+    chrome.storage.local.get('datosTranferManual', function (result) {
+      const datosTranferManual = result.datosTranferManual;
+
+      if (datosTranferManual) {
+        const datosTranferManualNum = Object.keys(datosTranferManual).length;
+        console.log('Se encontraron datos guardados:', datosTranferManualNum, datosTranferManual);
+
+        contador(datosTranferManualNum);
+        insertarDatos(datosTranferManual);
+      } else {
+        console.log('No se encontraron datos guardados.');
+      }
+    });
+  } else {
+    console.error('chrome.storage no está disponible.');
+  }
+
+  document.getElementById('pausar').addEventListener('click', () => {
+    pause = false;
+
+    if (chrome.storage) {
+      chrome.storage.local.set({ pauseChrome: pause }, () => {
+        console.log('Pause Set2:', pause);
+      });
+    } else {
+      console.error('chrome.storage no está disponible.');
+    }
+  });
+}
+
+function alertaCanceladora() {
+  const tiempoEspera = 250;
+
+  // Mostrar una alerta que permita al usuario cancelar la ejecución de la función
+  const confirmacion = confirm('¿Quieres cancelar?');
+  if (confirmacion) {
+    if (chrome.storage) {
+      chrome.storage.local.remove('datosAdjustment', function () {
+        console.log('datos borrados correctamente.');
+        setTimeout(() => {
+          window.location.reload();
+        }, tiempoEspera);
+      });
+    } else {
+      console.error('chrome.storage no está disponible.');
+    }
+  } else {
+    console.log('La función no se ha ejecutado.');
   }
 }
 
