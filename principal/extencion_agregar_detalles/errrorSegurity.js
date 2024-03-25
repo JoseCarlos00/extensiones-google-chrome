bfdb85616f787fbf2aef1795cde9e54f512ec78d
JoseCarlos00 <101861240+JoseCarlos00@users.noreply.github.com>
2024-03-25 14:14:37 -0600

    fix: error segurity: cerar venrana despues de enviar datos

--- principal/extencion_agregar_detalles/errrorSegurity.js
+++ principal/extencion_agregar_detalles/errrorSegurity.js
@@ -1,6 +1,10 @@
 function inicio() {
   console.log('errrorSegurity.js');
-  chrome.runtime.sendMessage({ action: 'datos_no_encontrados_desde_detail' });
+  chrome.runtime.sendMessage({
+    action: 'datos_no_encontrados_desde_detail',
+    data: 'Security access violation',
+  });
+  setTimeout(window.close, 50);
 }
 
 window.addEventListener('load', inicio, { once: true });
