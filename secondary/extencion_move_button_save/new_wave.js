34850a03078fb8b4bed3fa7a146a1a939b19553d
JoseCarlos00 <101861240+JoseCarlos00@users.noreply.github.com>
2024-04-03 11:12:53 -0600

    refactor: Mover boton Save New Wave

---
+++ secondary/extencion_move_button_save/new_wave.js
@@ -0,0 +1,38 @@
+function inicio() {
+  let altura = '';
+
+  const anchoElemento1 = document.querySelector(
+    '#ScreenGroupSubAccordion12898 > div:nth-child(2) > div:nth-child(1)'
+  );
+  const anchoElemento2 = document.querySelector('#ScreenControlToggleSwitch37984 > div');
+
+  setTimeout(() => {
+    // Auto realise
+    document.querySelector('#ScreenControlToggleSwitch37984 > div > div > div').click();
+  }, 1000);
+
+  setTimeout(() => {
+    document
+      .querySelector('#NewWaveMenu > li.dropdownaction.pull-right.menubutton.menubuttonsave')
+      .classList.add('my-botton-save');
+    document
+      .querySelector('#NewWaveMenu > li.dropdownaction.pull-right.menubutton.menubuttonsave')
+      .classList.remove('pull-right');
+
+    if (anchoElemento1 && anchoElemento2) {
+      altura = Number(anchoElemento1.offsetWidth) + Number(anchoElemento2.offsetWidth) + 15;
+
+      if (
+        document.querySelector(
+          '#NewWaveMenu > li.dropdownaction.menubutton.menubuttonsave.my-botton-save'
+        )
+      ) {
+        document.querySelector(
+          '#NewWaveMenu > li.dropdownaction.menubutton.menubuttonsave.my-botton-save'
+        ).style.left = `${altura}px`;
+      }
+    }
+  }, 2000);
+}
+
+window.addEventListener('load', inicio, { once: true });
