9502e80bd5ec7abf8918aa259d9ed85a55c526e8
JoseCarlos00 <101861240+JoseCarlos00@users.noreply.github.com>
2024-07-15 11:07:46 -0600

    feat: doock door: terminate

--- secondary/extencion_dock_door/content.js
+++ secondary/extencion_dock_door/content.js
@@ -13,34 +13,158 @@ const modalHTML = `
           class="fa-primary"></path>
       </svg>
     </button>
-    
+
+     <table id="content">
+    <thead>
+      <tr>
+        <th colspan="5" align="center">EMB</th>
+        <th colspan="2" align="center">Otras</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>EMB-01</td>
+        <td>EMB-16</td>
+        <td>EMB-31</td>
+        <td>EMB-46</td>
+        <td>EMB-61</td>
+        <td>AMZ-01</td>
+        <td>MARIANO CLIENTES</td>
+      </tr>
+      <tr>
+        <td>EMB-02</td>
+        <td>EMB-17</td>
+        <td>EMB-32</td>
+        <td>EMB-47</td>
+        <td>EMB-62</td>
+        <td>AMZ-02</td>
+        <td>MAY-01</td>
+      </tr>
+      <tr>
+        <td>EMB-03</td>
+        <td>EMB-18</td>
+        <td>EMB-33</td>
+        <td>EMB-48</td>
+        <td>EMB-63</td>
+        <td>DOCK-01</td>
+        <td>MAY-02</td>
+      </tr>
+      <tr>
+        <td>EMB-04</td>
+        <td>EMB-19</td>
+        <td>EMB-34</td>
+        <td>EMB-49</td>
+        <td>EMB-64</td>
+        <td>DOCK-02</td>
+        <td>ML-01</td>
+      </tr>
+      <tr>
+        <td>EMB-05</td>
+        <td>EMB-20</td>
+        <td>EMB-35</td>
+        <td>EMB-50</td>
+        <td>EMB-65</td>
+        <td>DOCK-03</td>
+        <td>ML-02</td>
+      </tr>
+      <tr>
+        <td>EMB-06</td>
+        <td>EMB-21</td>
+        <td>EMB-36</td>
+        <td>EMB-51</td>
+        <td>EMB-66</td>
+        <td>DOCK-04</td>
+        <td>TUL-01</td>
+      </tr>
+      <tr>
+        <td>EMB-07</td>
+        <td>EMB-22</td>
+        <td>EMB-37</td>
+        <td>EMB-52</td>
+        <td>EMB-67</td>
+        <td>EXT-01</td>
+        <td>TUL-02</td>
+      </tr>
+      <tr>
+        <td>EMB-08</td>
+        <td>EMB-23</td>
+        <td>EMB-38</td>
+        <td>EMB-53</td>
+        <td>EMB-68</td>
+        <td>EXT-02</td>
+        <td>VIRTUAL-02</td>
+      </tr>
+      <tr>
+        <td>EMB-09</td>
+        <td>EMB-24</td>
+        <td>EMB-39</td>
+        <td>EMB-54</td>
+        <td>EMB-69</td>
+        <td>EXT-03</td>
+        <td></td>
+      </tr>
+      <tr>
+        <td>EMB-10</td>
+        <td>EMB-25</td>
+        <td>EMB-40</td>
+        <td>EMB-55</td>
+        <td>EMB-70</td>
+        <td>INT-01</td>
+        <td></td>
+      </tr>
+      <tr>
+        <td>EMB-11</td>
+        <td>EMB-26</td>
+        <td>EMB-41</td>
+        <td>EMB-56</td>
+        <td>EMB-71</td>
+        <td>INT-02</td>
+        <td></td>
+      </tr>
+      <tr>
+        <td>EMB-12</td>
+        <td>EMB-27</td>
+        <td>EMB-42</td>
+        <td>EMB-57</td>
+        <td>EMB-72</td>
+        <td>INT-03</td>
+        <td></td>
+      </tr>
+      <tr>
+        <td>EMB-13</td>
+        <td>EMB-28</td>
+        <td>EMB-43</td>
+        <td>EMB-58</td>
+        <td>EMB-73</td>
+        <td>LIMPIEZA</td>
+        <td></td>
+      </tr>
+      <tr>
+        <td>EMB-14</td>
+        <td>EMB-29</td>
+        <td>EMB-44</td>
+        <td>EMB-59</td>
+        <td>EMB-74</td>
+        <td>MAE-01</td>
+        <td></td>
+      </tr>
+      <tr>
+        <td>EMB-15</td>
+        <td>EMB-30</td>
+        <td>EMB-45</td>
+        <td>EMB-60</td>
+        <td>EMB-75</td>
+        <td>MAR-01</td>
+        <td></td>
+      </tr>
+    </tbody>
+  </table>
 
     </div>
   </div>
 
 </section>
 `;
-
-function inicio() {
-  console.log('[Inventory Insight Modal]');
-  const ul =
-    document.querySelector('#topNavigationBar > nav > ul.collapsepane.nav.navbar-nav') ?? null;
-
-  const li = `
-    <li class="navdetailpane visible-sm visible-md visible-lg">
-      <a id='openModalBtn' href="#" data-toggle="detailpane" class="navimageanchor visiblepane" aria-label="Abrir Modal" data-balloon-pos="right">
-        <i class="far fa-clipboard navimage"></i>
-      </a>
-    </li>
-    `;
-
-  if (!ul) return;
-
-  ul.insertAdjacentHTML('beforeend', li);
-  document.querySelector('body').insertAdjacentHTML('beforeend', alertHtml);
-
-  modalFunction();
-}
 
 function inicio() {
   console.log('[Inventory Insight Modal]');
@@ -83,12 +207,14 @@ function modalFunction() {
   });
 }
 
-function setEventModal(elements) {
+function setEventModalOpenClose(elements) {
   const { btnOpen, btnClose, modal } = elements;
 
   // Cuando el usuario hace clic en el botón, abre el modal
   btnOpen.addEventListener('click', function () {
     modal.style.display = 'block';
+
+    insertDoockNotAvailable();
   });
 
   // Cuando el usuario hace clic en <span> (x), cierra el modal
@@ -100,19 +226,14 @@ function setEventModal(elements) {
 function setEventListener(elements) {
   const { modal } = elements;
 
-  setEventModal(elements);
+  setEventModalOpenClose(elements);
 
   // Cuando el usuario hace clic fuera del modal, ciérralo
   window.addEventListener('click', function (e) {
     const element = e.target;
-    const nodeName = e.target.nodeName;
 
-    if (element == modal) {
+    if (element === modal) {
       modal.style.display = 'none';
-    }
-
-    if (nodeName === 'INPUT') {
-      element.select();
     }
   });
 
@@ -123,6 +244,74 @@ function setEventListener(elements) {
       }
     }
   });
+}
+
+function cleanClass(clase) {
+  return new Promise((resolve, reject) => {
+    try {
+      const clases = document.querySelectorAll(`table#content .${clase}`);
+      console.log('cleanClass:', clases);
+
+      if (clases.length === 0) {
+        resolve(); // Resuelve la promesa incluso si no hay elementos
+        return;
+      }
+
+      clases.forEach(td => td.classList.remove(clase));
+      resolve(); // Resuelve la promesa después de eliminar la clase
+    } catch (error) {
+      reject(error); // Rechaza la promesa en caso de error
+    }
+  });
+}
+
+function getDoockDoor() {
+  let door = [];
+
+  return new Promise((resolve, reject) => {
+    const dock_doors = document.querySelectorAll(
+      'td[aria-describedby="ListPaneDataGrid_DOCK_DOOR_LOCATION"]'
+    );
+
+    if (dock_doors.length === 0) {
+      reject('No se encontraron los elementos: ListPaneDataGrid_DOCK_DOOR_LOCATION');
+      return;
+    }
+
+    dock_doors.forEach(doorElement => {
+      const content = doorElement.innerHTML;
+
+      if (content) {
+        door.push(content);
+      }
+    });
+
+    resolve(door);
+  });
+}
+
+async function insertDoockNotAvailable() {
+  console.log('[insertDoockNotAvailable]');
+
+  try {
+    const table = document.getElementById('content');
+    if (!table) return; // Asegurarse de que la tabla existe
+
+    const rows = Array.from(table.querySelectorAll('tbody tr td'));
+    if (rows.length === 0) return; // Asegurarse de que hay filas en la tabla
+
+    const door = await getDoockDoor();
+    await cleanClass('not-available');
+
+    rows.forEach(td => {
+      const content = td.innerHTML;
+      if (door.includes(content)) {
+        td.classList.add('not-available');
+      }
+    });
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 window.addEventListener('load', inicio);
