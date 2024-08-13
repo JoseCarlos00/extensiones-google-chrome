e67c4151339af7d8aa0f2d4ec7d0cb03a5e90d60
JoseCarlos00 <101861240+JoseCarlos00@users.noreply.github.com>
2024-08-12 23:08:48 -0600

    refactor: Modal SQL: creacion de clases

---
+++ secondary/extencion_modal_crear_SQL/ModalHandler.js
@@ -0,0 +1,141 @@
+class ModalHandler {
+  constructor(modal) {
+    if (!modal) {
+      throw new Error('No se encontró el modal para abrir');
+    }
+
+    this.modal = modal;
+    this.selectors = {
+      internalContainerNum: "td[aria-describedby='ListPaneDataGrid_INTERNAL_CONTAINER_NUM']",
+      containerId: "td[aria-describedby='ListPaneDataGrid_CONTAINER_ID']",
+      tbody: '#ListPaneDataGrid > tbody',
+      internalContainerNumElement: 'internal-container-id-num',
+      internalParentContainerNumElement: 'internal-parent-container-id-num',
+      internalContainerNumbersElement: 'numbers-internals-containers',
+      containerIdElement: 'container-id',
+      parentContainerIdElement: 'parent-container-id',
+    };
+    this.internalData = {
+      internalNumContainerId: '',
+      internalNumParentContainerId: [],
+      LP: '',
+      internalsNumbers: [],
+    };
+    this.internalContainerNumElement = null;
+    this.internalParentContainerNumElement = null;
+    this.internalContainerNumbersElement = null;
+    this.containerIdElement = null;
+    this.parentContainerIdElement = null;
+  }
+
+  async handleOpenModal() {
+    try {
+      await this.processInternalTableData();
+      await this.setElementValues();
+      await this.openModal();
+    } catch (error) {
+      console.error(`Error en handleOpenModal: ${error.message}`);
+    }
+  }
+
+  /**
+   * Initializes the variables by selecting DOM elements.
+   * Throws an error if any element is not found.
+   */
+  async initialVariables() {
+    // Select elements
+    this.internalContainerNumElement = document.getElementById(
+      this.selectors.internalContainerNumElement
+    );
+    this.internalParentContainerNumElement = document.getElementById(
+      this.selectors.internalParentContainerNumElement
+    );
+    this.internalContainerNumbersElement = document.getElementById(
+      this.selectors.internalContainerNumbersElement
+    );
+    this.containerIdElement = document.getElementById(this.selectors.containerIdElement);
+    this.parentContainerIdElement = document.getElementById(
+      this.selectors.parentContainerIdElement
+    );
+
+    // Check if all elements are selected
+    await this.verifyAllElement();
+  }
+
+  async verifyAllElement() {
+    if (
+      !this.internalContainerNumElement ||
+      !this.internalParentContainerNumElement ||
+      !this.internalContainerNumbersElement ||
+      !this.containerIdElement ||
+      !this.parentContainerIdElement
+    ) {
+      throw new Error('No se encontraron los elementos para inicializar los variables');
+    }
+  }
+
+  async openModal() {
+    this.modal.style.display = 'block';
+    console.log('Modal abierto');
+  }
+
+  async processInternalTableData() {
+    const tbody = document.querySelector('#ListPaneDataGrid > tbody');
+    if (!tbody) {
+      throw new Error('No se encontró <tbody>');
+    }
+
+    const rows = Array.from(tbody.querySelectorAll('tr'));
+    await this.setInternalData(rows);
+  }
+
+  async cleanInternalData() {
+    this.internalData.internalNumContainerId = '';
+    this.internalData.internalNumParentContainerId.length = 0;
+    this.internalData.internalsNumbers.length = 0;
+    this.internalData.LP = '';
+  }
+
+  async setInternalData(rows) {
+    const { internalContainerNum, containerId } = this.selectors;
+
+    if (rows.length === 0) {
+      throw new Error('No se encontraron filas en el <tbody>');
+    }
+
+    await this.cleanInternalData();
+
+    rows.forEach(row => {
+      const internalContainerNumElement = row.querySelector(internalContainerNum);
+      const containerIdElement = row.querySelector(containerId);
+
+      if (internalContainerNumElement && containerIdElement) {
+        const containerIdText = containerIdElement.textContent.trim();
+        const internalNumText = internalContainerNumElement.textContent.trim();
+
+        if (containerIdText && internalNumText) {
+          this.internalData.internalNumContainerId = internalNumText;
+          this.internalData.internalsNumbers.push(internalNumText);
+        } else if (!containerIdText && internalNumText) {
+          this.internalData.internalNumParentContainerId.push(internalNumText);
+          this.internalData.internalsNumbers.push(internalNumText);
+        }
+      }
+    });
+
+    console.log('internalData:', this.internalData); // Considera eliminar esto en producción
+  }
+
+  async setElementValues() {
+    await this.verifyAllElement();
+
+    this.internalContainerNumElement.textContent = `'${this.internalData.internalNumContainerId}'`;
+    this.internalParentContainerNumElement.textContent =
+      this.internalData.internalNumParentContainerId.map(i => `'${i}'`).join(', ');
+    this.internalContainerNumbersElement.textContent = this.internalData.internalsNumbers
+      .map(i => `'${i}'`)
+      .join(', ');
+  }
+}
+
+// const modalHandler = new ModalHandler(modalElement);
---
+++ secondary/extencion_modal_crear_SQL/ModalManager.js
@@ -0,0 +1,185 @@
+console.log('[Shipping container Insight Modal]');
+
+class ModalManager {
+  constructor() {
+    this.modalElement = null;
+    this.btnOpen = null;
+    this.btnClose = null;
+    this.modalHandler = null;
+  }
+
+  async initialize() {
+    try {
+      await this.insertBtnOpenModal();
+      await this.insertModal();
+      this.modalFunction();
+    } catch (error) {
+      console.error('Error al crear el modal:', error);
+    }
+  }
+
+  async insertBtnOpenModal() {
+    const li = `
+    <li class="navdetailpane visible-sm visible-md visible-lg">
+      <a id='openModalBtn' href="#" data-toggle="detailpane" class="navimageanchor visiblepane" aria-label="Crear Sentemcia SQL" data-balloon-pos="right">
+        <i class="far fa-plus navimage"></i>
+      </a>
+    </li>
+    `;
+
+    const alertHtml = `
+    <div id="alerta-copy" aria-live="polite"
+      style="bottom: 40px; position: fixed; left: 0px; width: 100%; display: none; z-index: 1000000; padding: 4px; opacity: 0; transition-property: opacity, transform; transition-duration: 270ms; transition-timing-function: ease;">
+      <div
+        style="background: rgb(47, 47, 47); color: rgb(211, 211, 211); border-radius: 8px; padding: 11px 16px; box-shadow: rgba(15, 15, 15, 0.1) 0px 0px 0px 1px,   rgba(15, 15, 15, 0.2) 0px 5px 10px,   rgba(15, 15, 15, 0.4) 0px 15px 40px; margin: 0px auto; font-size: 14px; display: flex; align-items: center;">
+        Copiado al portapapeles
+        <div style="margin-left: 4px; margin-right: -4px"></div>
+      </div>
+    </div>
+    `;
+
+    return new Promise(resolve => {
+      const ul = document.querySelector('#topNavigationBar > nav > ul.collapsepane.nav.navbar-nav');
+
+      if (!ul) {
+        throw new Error('No se encontró el elemento ul');
+      }
+
+      ul.insertAdjacentHTML('beforeend', li);
+
+      const body = document.querySelector('body');
+      body && body.insertAdjacentHTML('beforeend', alertHtml);
+
+      setTimeout(resolve, 50);
+    });
+  }
+
+  async insertModal() {
+    const btnCopy = `
+  <button class="btn-copiar"
+  style="position: absolute;top: 3px;right: 1px;z-index: 1;color: rgba(255, 255, 255, 0.443);display: flex;align-items: center;justify-content: flex-end;height: 25px;font-size: 11.5px;opacity: 0;transition: opacity 300ms ease-in;border: none;">
+  <div
+    style="color: rgba(255, 255, 255, 0.443); display: flex; align-items: center; justify-content: center; font-size: 12px; margin-top: 4px; margin-right: 4px;">
+    <div role="button" tabindex="0"
+      style="user-select: none; transition: background 20ms ease-in; cursor: pointer; display: inline-flex; align-items: center; white-space: nowrap; height: 25px; border-radius: 4px 0px 0px 4px; font-size: 11.5px; line-height: 1.2; padding: 4px 6px; color: rgba(255, 255, 255, 0.81); background: rgb(37, 37, 37); font-weight: 400;"
+      class="copy">
+      <svg role="graphics-symbol" viewBox="0 0 14 16"
+        style="width: 16px; height: 16px; display: block; fill: #fff; flex-shrink: 0; padding-right: 4px;"><path d="M2.404 15.322h5.701c1.26 0 1.887-.662 1.887-1.927V12.38h1.154c1.254 0 1.91-.662 1.91-1.928V5.555c0-.774-.158-1.266-.626-1.74L9.512.837C9.066.387 8.545.21 7.865.21H5.463c-1.254 0-1.91.662-1.91 1.928v1.084H2.404c-1.254 0-1.91.668-1.91 1.933v8.239c0 1.265.656 1.927 1.91 1.927zm7.588-6.62c0-.792-.1-1.161-.592-1.665L6.225 3.814c-.452-.462-.844-.58-1.5-.591V2.215c0-.533.28-.832.843-.832h2.38v2.883c0 .726.386 1.113 1.107 1.113h2.83v4.998c0 .539-.276.832-.844.832H9.992V8.701zm-.79-4.29c-.206 0-.288-.088-.288-.287V1.594l2.771 2.818H9.201zM2.503 14.15c-.563 0-.844-.293-.844-.832V5.232c0-.539.281-.837.85-.837h1.91v3.187c0 .85.416 1.26 1.26 1.26h3.14v4.476c0 .54-.28.832-.843.832H2.504zM5.79 7.816c-.24 0-.346-.105-.346-.345V4.547l3.223 3.27H5.791z"></path>
+      </svg>
+      Copiar
+    </div>
+  </div>
+  </button>`;
+
+    const modalHTML = `
+      <section class="modal-container">
+        <div id="myModal" class="modal">
+          <div class="modal-content">
+  
+          <button type="button" aria-label="Close" data-balloon-pos="left" class="close">
+            <svg aria-hidden="true" focusable="false" data-prefix="fad" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="fa-circle-xmark">
+              <path fill="currentColor"
+                d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM175 175c9.4-9.4 24.6-9.4 33.9 0l47 47 47-47c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9l-47 47 47 47c9.4 9.4 9.4 24.6 0 33.9s-24.6 9.4-33.9 0l-47-47-47 47c-9.4 9.4-24.6 9.4-33.9 0s-9.4-24.6 0-33.9l47-47-47-47c-9.4-9.4-9.4-24.6 0-33.9z"
+                class="fa-secondary"></path>
+              <path fill="currentColor"
+                d="M209 175c-9.4-9.4-24.6-9.4-33.9 0s-9.4 24.6 0 33.9l47 47-47 47c-9.4 9.4-9.4 24.6 0 33.9s24.6 9.4 33.9 0l47-47 47 47c9.4 9.4 24.6 9.4 33.9 0s9.4-24.6 0-33.9l-47-47 47-47c9.4-9.4 9.4-24.6 0-33.9s-24.6-9.4-33.9 0l-47 47-47-47z"
+                class="fa-primary"></path>
+            </svg>
+          </button>
+  
+  <pre class="postition-relative">${btnCopy}<code class="language-sql hljs" data-highlighted="yes"><span class="hljs-keyword">UPDATE</span> shipping_container
+    <span class="hljs-keyword">SET</span> 
+      container_id <span class="hljs-operator">=</span> <span class="hljs-keyword">CASE</span> <span class="hljs-keyword">WHEN</span> internal_container_num <span class="hljs-operator">=</span> <span class="hljs-string" id="internal-container-id-num"></span> <span class="hljs-keyword">THEN</span> <span class="hljs-string" id="container-id" contenteditable="true">'FMA0002975623'</span> <span class="hljs-keyword">ELSE</span> <span class="hljs-keyword">null</span> <span class="hljs-keyword">END</span>,
+      parent_container_id <span class="hljs-operator">=</span> <span class="hljs-keyword">CASE</span> <span class="hljs-keyword">WHEN</span> internal_container_num <span class="hljs-keyword">IN</span> (<span class="hljs-string" id="internal-parent-container-id-num"></span>) <span class="hljs-keyword">THEN</span> <span class="hljs-string" id="parent-container-id" contenteditable="true">'FMA0002975623'</span> <span class="hljs-keyword">ELSE</span> <span class="hljs-keyword">null</span> <span class="hljs-keyword">END</span>
+    <span class="hljs-keyword">WHERE</span> internal_container_num <span class="hljs-keyword">IN</span> (<span class="hljs-string" id="numbers-internals-containers"></span>);</code>
+  </pre>
+          
+          </div>
+        </div>
+  
+      </section>
+  `;
+
+    return new Promise((resolve, reject) => {
+      const body = document.querySelector('body');
+
+      if (!body) return reject('No se encontro elemento <body> a insertar el Modal');
+
+      body.insertAdjacentHTML('beforeend', modalHTML);
+      setTimeout(resolve, 50);
+    });
+  }
+
+  modalFunction() {
+    try {
+      this.modalElement = document.getElementById('myModal');
+      this.btnOpen = document.getElementById('openModalBtn');
+      this.btnClose = document.querySelector('.modal-container .close');
+
+      this.modalHandler = new ModalHandler(this.modalElement);
+      this.setEventListeners();
+    } catch (error) {
+      console.error('Error al inicializar los eventos del modal:', error);
+    }
+  }
+
+  setEventListeners() {
+    if (!this.modalElement || !this.btnOpen || !this.btnClose) {
+      throw new Error('No se encontraron los elementos necesarios para inicializar el modal');
+    }
+
+    this.modalHandler.initialVariables();
+
+    this.btnOpen.addEventListener('click', () => {
+      this.modalHandler.handleOpenModal();
+    });
+
+    this.btnClose.addEventListener('click', () => {
+      this.closeModal();
+    });
+
+    window.addEventListener('click', e => {
+      if (e.target === this.modalElement) {
+        this.closeModal();
+      }
+    });
+
+    window.addEventListener('keydown', e => {
+      if (e.key === 'Escape' && this.modalElement.style.display === 'block') {
+        this.closeModal();
+      }
+    });
+  }
+
+  openModal() {
+    this.modalElement.style.display = 'block';
+  }
+
+  closeModal() {
+    this.modalElement.style.display = 'none';
+  }
+}
+
+async function copy(textoACopiar) {
+  try {
+    await navigator.clipboard.writeText(textoACopiar);
+    const alerta = document.querySelector('#alerta-copy');
+
+    alerta && (alerta.style.opacity = 1);
+    setTimeout(() => {
+      alerta && (alerta.style.opacity = 0);
+    }, 4000);
+  } catch (err) {
+    console.error('Error al copiar al portapapeles:', err);
+    alert('Error al copiar al portapapeles:');
+  }
+}
+
+window.addEventListener(
+  'load',
+  async () => {
+    const modalManager = new ModalManager();
+    await modalManager.initialize();
+  },
+  { once: true }
+);
