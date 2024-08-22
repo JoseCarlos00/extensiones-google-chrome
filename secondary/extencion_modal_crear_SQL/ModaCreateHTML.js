58c5b246ef294d22f503ea9fcb9c9e1c123202dd
JoseCarlos00 <101861240+JoseCarlos00@users.noreply.github.com>
2024-08-22 14:26:38 -0600

    feat: Modal SQL: MidalManagerInventory: modificar closeModal

--- secondary/extencion_modal_crear_SQL/ModaCreateHTML.js
+++ secondary/extencion_modal_crear_SQL/ModaCreateHTML.js
@@ -3,6 +3,55 @@ class ModalCreateHTML {
     this.contenModal = null;
   }
 
+  async _createButtonClose() {
+    const buttonClose = document.createElement('button');
+    buttonClose.className = 'close';
+    buttonClose.type = 'button';
+    buttonClose.setAttribute('aria-label', 'Close');
+    buttonClose.setAttribute('data-balloon-pos', 'left');
+
+    buttonClose.innerHTML = `
+      <svg aria-hidden="true" focusable="false" data-prefix="fad" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="fa-circle-xmark">
+        <path fill="currentColor"
+          d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM175 175c9.4-9.4 24.6-9.4 33.9 0l47 47 47-47c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9l-47 47 47 47c9.4 9.4 9.4 24.6 0 33.9s-24.6 9.4-33.9 0l-47-47-47 47c-9.4 9.4-24.6 9.4-33.9 0s-9.4-24.6 0-33.9l47-47-47-47c-9.4-9.4-9.4-24.6 0-33.9z"
+          class="fa-secondary">
+        </path>
+        <path fill="currentColor"
+          d="M209 175c-9.4-9.4-24.6-9.4-33.9 0s-9.4 24.6 0 33.9l47 47-47 47c-9.4 9.4-9.4 24.6 0 33.9s24.6 9.4 33.9 0l47-47 47 47c9.4 9.4 24.6 9.4 33.9 0s9.4-24.6 0-33.9l-47-47 47-47c9.4-9.4 9.4-24.6 0-33.9s-24.6-9.4-33.9 0l-47 47-47-47z"
+        class="fa-primary">
+        </path>
+      </svg>
+      `;
+
+    return buttonClose;
+  }
+
+  async _createDivModal() {
+    const divModal = document.createElement('div');
+    divModal.className = 'modal';
+    divModal.id = 'myModal';
+
+    return divModal;
+  }
+
+  async _createSection() {
+    const section = document.createElement('section');
+    section.className = 'modal-container';
+
+    return section;
+  }
+
+  async _createContentModal() {
+    const contentModal = document.createElement('div');
+    contentModal.className = 'modal-content';
+
+    return contentModal;
+  }
+
+  /**
+   * Inserta contenido en <contenModal>
+   * @param  content Recibe un Elemento html o Un String
+   */
   async insertContenModal(content) {
     const { contenModal } = this;
 
@@ -26,61 +75,16 @@ class ModalCreateHTML {
     }
   }
 
-  async createButtonClose() {
-    const buttonClose = document.createElement('button');
-    buttonClose.className = 'close';
-    buttonClose.type = 'button';
-    buttonClose.setAttribute('aria-label', 'Close');
-    buttonClose.setAttribute('data-balloon-pos', 'left');
-
-    buttonClose.innerHTML = `
-      <svg aria-hidden="true" focusable="false" data-prefix="fad" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="fa-circle-xmark">
-        <path fill="currentColor"
-          d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM175 175c9.4-9.4 24.6-9.4 33.9 0l47 47 47-47c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9l-47 47 47 47c9.4 9.4 9.4 24.6 0 33.9s-24.6 9.4-33.9 0l-47-47-47 47c-9.4 9.4-24.6 9.4-33.9 0s-9.4-24.6 0-33.9l47-47-47-47c-9.4-9.4-9.4-24.6 0-33.9z"
-          class="fa-secondary">
-        </path>
-        <path fill="currentColor"
-          d="M209 175c-9.4-9.4-24.6-9.4-33.9 0s-9.4 24.6 0 33.9l47 47-47 47c-9.4 9.4-9.4 24.6 0 33.9s24.6 9.4 33.9 0l47-47 47 47c9.4 9.4 24.6 9.4 33.9 0s9.4-24.6 0-33.9l-47-47 47-47c9.4-9.4 9.4-24.6 0-33.9s-24.6-9.4-33.9 0l-47 47-47-47z"
-        class="fa-primary">
-        </path>
-      </svg>
-      `;
-
-    return buttonClose;
-  }
-
-  async createDivModal() {
-    const divModal = document.createElement('div');
-    divModal.className = 'modal';
-    divModal.id = 'myModal';
-
-    return divModal;
-  }
-
-  async createSection() {
-    const section = document.createElement('section');
-    section.className = 'modal-container';
-
-    return section;
-  }
-
-  async createContentModal() {
-    const contentModal = document.createElement('div');
-    contentModal.className = 'modal-content';
-
-    return contentModal;
-  }
-
   /**
    * Crea una estructura html para un modal
    * @returns Elemento <section>
    */
   async createModaElement() {
     try {
-      const section = await this.createSection();
-      const modal = await this.createDivModal();
-      const contentModal = await this.createContentModal();
-      const buttonClose = await this.createButtonClose();
+      const section = await this._createSection();
+      const modal = await this._createDivModal();
+      const contentModal = await this._createContentModal();
+      const buttonClose = await this._createButtonClose();
 
       contentModal.appendChild(buttonClose);
       modal.appendChild(contentModal);
--- secondary/extencion_modal_crear_SQL/inventory_insight/modalContent.js
+++ secondary/extencion_modal_crear_SQL/inventory_insight/modalContent.js
@@ -5,7 +5,7 @@ async function getHtmlContent() {
     <div
       style="color: rgba(255, 255, 255, 0.443); display: flex; align-items: center; justify-content: center; font-size: 12px; margin-top: 4px;">
       <div role="button" tabindex="0"
-        style="user-select: none; transition: background 20ms ease-in; cursor: pointer; display: inline-flex; align-items: center; white-space: nowrap; height: 25px; border-radius:  0px 4px 4px 0px; font-size: 11.5px; line-height: 1.2; padding: 4px 6px; color: rgba(255, 255, 255, 0.81); background: rgb(37, 37, 37); font-weight: 400;"
+        style="user-select: none; transition: background 20ms ease-in; cursor: pointer; display: inline-flex; align-items: center; white-space: nowrap; height: 25px; border-radius: 4px; font-size: 11.5px; line-height: 1.2; padding: 4px 6px; color: rgba(255, 255, 255, 0.81); background: rgb(37, 37, 37); font-weight: 400;"
         class="copy">
         <svg role="graphics-symbol" viewBox="0 0 14 16"
           style="width: 16px; height: 16px; display: block; fill: #fff; flex-shrink: 0; padding-right: 4px;"><path d="M2.404 15.322h5.701c1.26 0 1.887-.662 1.887-1.927V12.38h1.154c1.254 0 1.91-.662 1.91-1.928V5.555c0-.774-.158-1.266-.626-1.74L9.512.837C9.066.387 8.545.21 7.865.21H5.463c-1.254 0-1.91.662-1.91 1.928v1.084H2.404c-1.254 0-1.91.668-1.91 1.933v8.239c0 1.265.656 1.927 1.91 1.927zm7.588-6.62c0-.792-.1-1.161-.592-1.665L6.225 3.814c-.452-.462-.844-.58-1.5-.591V2.215c0-.533.28-.832.843-.832h2.38v2.883c0 .726.386 1.113 1.107 1.113h2.83v4.998c0 .539-.276.832-.844.832H9.992V8.701zm-.79-4.29c-.206 0-.288-.088-.288-.287V1.594l2.771 2.818H9.201zM2.503 14.15c-.563 0-.844-.293-.844-.832V5.232c0-.539.281-.837.85-.837h1.91v3.187c0 .85.416 1.26 1.26 1.26h3.14v4.476c0 .54-.28.832-.843.832H2.504zM5.79 7.816c-.24 0-.346-.105-.346-.345V4.547l3.223 3.27H5.791z"></path>
@@ -13,7 +13,8 @@ async function getHtmlContent() {
         Copiar
       </div>
     </div>
-    </button>`;
+    </button>
+    `;
 
   const inputRadios = `
    <div class="radio-container">
@@ -27,10 +28,12 @@ async function getHtmlContent() {
           <span class="name">Item - Location</span>
         </label>
       </div>
-    </div>`;
+    </div>
+    `;
 
   const inputChecks = `
-    <div class="opcs-btn-container">
+    <div class="opcs-btn-container position-relative">
+    ${btnCopy}
       <input class="opc-btn" id="opc-oh" type="checkbox" checked="true" data-type="OH" />
       <label class="opc-label" for="opc-oh">OH</label>
 
@@ -42,79 +45,56 @@ async function getHtmlContent() {
 
       <input class="opc-btn" id="opc-su" type="checkbox" data-type="SU" />
       <label class="opc-label" for="opc-su">SU</label>
-    </div>`;
-
-  const internalNum = `
-    <div class="internal-num-code code-content language-sql hljs">
-      <span class="hljs-keyword">UPDATE</span> location_inventory
-      <br>&emsp;<span id="element-to-insert" class="hljs-keyword">SET</span>
-
-      <div class="container-type" data-type="OH">
-        &emsp;&ensp;<span>ON_HAND_QTY</span> <span class="hljs-operator">=</span>
-        <input type="number" value="" />
-      </div>
-
-      <div class="container-type" data-type="AL">
-        &emsp;&ensp;<span>ALLOCATED_QTY</span> <span class="hljs-operator">=</span> <input type="number" value="" />
-      </div>
-
-      <div class="container-type" data-type="IT">
-        &emsp;&ensp;<span>IN_TRANSIT_QTY</span> <span class="hljs-operator">=</span> <input type="number" value="" />
-      </div>
-
-      <div class="container-type" data-type="SU">
-        &emsp;&ensp;<span>SUSPENSE_QTY </span><span class="hljs-operator">=</span> <input type="number" value="" />
-      </div>
-
-      <br>&emsp;<span class="hljs-keyword">WHERE</span> warehouse <span class="hljs-operator">=</span> 'Mariano'
-
-      <br>&emsp;<span class="hljs-keyword">AND</span>
-      internal_location_inv
-
-      <br>&emsp;<span class="hljs-keyword aling-top">IN</span><span class="aling-top">(</span><span
-        class="hljs-string">
-        <div contenteditable class="internal-inventory-numbers" id="internal-inventory-numbers"></div>
-      </span>);
-    </div>`;
-
-  const itemLoc = `
-    <div class="item-loc-code code-content language-sql hljs">
-      <span class="hljs-keyword">UPDATE</span> location_inventory
-      <br>&emsp;<span id="element-to-insert" class="hljs-keyword">SET</span>
-
-      <div class="container-type" data-type="OH">
-        &emsp;&ensp;<span>ON_HAND_QTY</span> <span class="hljs-operator">=</span>
-        <input type="number" value="" />
-      </div>
-
-      <div class="container-type" data-type="AL">
-        &emsp;&ensp;<span>ALLOCATED_QTY</span> <span class="hljs-operator">=</span> <input type="number" value="" />
-      </div>
-
-      <div class="container-type" data-type="IT">
-        &emsp;&ensp;<span>IN_TRANSIT_QTY</span> <span class="hljs-operator">=</span> <input type="number" value="" />
-      </div>
-
-      <div class="container-type" data-type="SU">
-        &emsp;&ensp;<span>SUSPENSE_QTY </span><span class="hljs-operator">=</span> <input type="number" value="" />
-      </div>
-
-      <br>&emsp;<span class="hljs-keyword">WHERE</span> warehouse <span class="hljs-operator">=</span> 'Mariano'
-
-      <br>&emsp;<span class="hljs-keyword">AND</span> location <span class="hljs-operator">=</span>
-      <input type="text" name="location" id="location">
-      <br>&emsp;<span class="hljs-keyword">AND</span> item <span class="hljs-operator">=</span> <input type="text"
-        name="item" id="item">
-
     </div>
+    `;
+
+  const codeContainer = `
+    <div class="code-container postition-relative language-sql hljs">
+
+      <span class="hljs-keyword">UPDATE</span> location_inventory
+      <br>&emsp;<span id="element-to-insert" class="hljs-keyword">SET</span>
+
+      <div class="container-type" data-type="OH">
+        &emsp;&ensp;<span>ON_HAND_QTY</span> <span class="hljs-operator">=</span>
+        <input type="number" value="" id="input_OH"/>
+      </div>
+
+      <div class="container-type" data-type="AL">
+        &emsp;&ensp;<span>ALLOCATED_QTY</span> <span class="hljs-operator">=</span> <input type="number" value="" id="input_AL"/>
+      </div>
+
+      <div class="container-type" data-type="IT">
+        &emsp;&ensp;<span>IN_TRANSIT_QTY</span> <span class="hljs-operator">=</span> <input type="number" value="" id="input_IT"/>
+      </div>
+
+      <div class="container-type" data-type="SU">
+        &emsp;&ensp;<span>SUSPENSE_QTY </span><span class="hljs-operator">=</span> <input type="number" value="" id="input_SU"/>
+      </div>
+
+      <br>&emsp;<span class="hljs-keyword">WHERE</span> warehouse <span class="hljs-operator">=</span> 'Mariano'
+
+      <div class="internal-num-code">
+        <br>&emsp;<span class="hljs-keyword">AND</span>
+        internal_location_inv
+        <br>&emsp;<span class="hljs-keyword aling-top">IN</span><span class="aling-top">(</span><span
+          class="hljs-string">
+          <div contenteditable class="internal-inventory-numbers" id="internal-inventory-numbers"></div>
+        </span>);
+      </div>
+
+      <div class="item-loc-code">
+        <br>&emsp;<span class="hljs-keyword">AND</span> location <span class="hljs-operator">=</span>
+        <input type="text" name="location" id="location">
+        <br>&emsp;<span class="hljs-keyword">AND</span> item <span class="hljs-operator">=</span> <input type="text"
+          name="item" id="item">
+      </div>
+
+    </div>  
   `;
 
   const containerMain = document.createElement('div');
-  containerMain.className = 'container-main'.className = 'main-code-container internal-num OH';
-  containerMain.innerHTML =
-    inputRadios +
-    inputChecks +
-    `<div class="code-container postition-relative">${internalNum + itemLoc}</div>`;
+  containerMain.className = 'main-code-container internal-num OH';
+  containerMain.innerHTML = inputRadios + inputChecks + codeContainer;
 
   const modal = new ModalCreateHTML();
   const modalHTML = await modal.createModaElement();
