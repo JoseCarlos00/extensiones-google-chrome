e48eacc473b7e1948c14aed0da15af4e3c659d2e
JoseCarlos00 <101861240+JoseCarlos00@users.noreply.github.com>
2024-08-26 16:34:52 -0600

    Update: Modal SQl: Obtener datos internos

--- secondary/extencion_modal_crear_SQL/shipment_line_insight/ModalHandler.js
+++ secondary/extencion_modal_crear_SQL/shipment_line_insight/ModalHandler.js
@@ -10,12 +10,62 @@
 class ModalHandler {
   constructor() {
     this.modal = null;
+    this.tbody = document.querySelector('#ListPaneDataGrid > tbody');
     this.internalDataSelector = {
       internalNumber: "td[aria-describedby='ListPaneDataGrid_INTERNAL_LOCATION_INV']",
     };
+    this.prefix = '#myModal .modal-content ';
+    this.internalShipmentNum = null;
+    this.status1 = null;
   }
 
-  async _openModal() {
+  #setinternalShipmentNum() {
+    this.internalShipmentNum = document.querySelector(`${this.prefix} #internal_shipment_line_num`);
+  }
+
+  #setStatus1() {
+    this.status1 = document.querySelector(`${this.prefix} #status1`);
+  }
+
+  async #getInternalData(rows) {
+    if (rows.length === 0) {
+      console.error('No se enontraron filas');
+      return;
+    }
+
+    const internalData = rows
+      .map(item => {
+        const internalNumber = item
+          .querySelector(this.internalDataSelector.internalNumber)
+          ?.textContent.trim();
+
+        return `'${internalNumber}'`;
+      })
+      .filter(Boolean)
+      .join('\n');
+
+    return internalData;
+  }
+
+  async #setInternalData() {
+    const internalData = await this.#getInternalData();
+  }
+
+  async #processInternalTableData() {
+    if (!this.tbody) {
+      throw new Error('No se encontro el elemento <tbody>');
+    }
+
+    const rows = Array.from(tbody.querySelectorAll('tr'));
+
+    if (rows.length === 0) {
+      throw new Error('No se encontraron filas en el <tbody>');
+    }
+
+    await this.#getInternalData(rows);
+  }
+
+  async #openModal() {
     this.modal.style.display = 'block';
   }
 
@@ -26,6 +76,8 @@ class ModalHandler {
       }
 
       this.modal = modal;
+      this.#setinternalShipmentNum();
+      this.#setStatus1();
     } catch (error) {
       console.error(`Error en setModalElement: ${error}`);
     }
@@ -33,7 +85,8 @@ class ModalHandler {
 
   async handleOpenModal() {
     try {
-      await this._openModal();
+      await this.#openModal();
+      this.status1.focus();
     } catch (error) {
       console.error(`Error en handleOpenModal: ${error}`);
     }
