4ad8c3f9fab4243cf0ecf195b66168ca2621016f
Jose Carlos AR <101861240+JoseCarlos00@users.noreply.github.com>
2024-11-08 22:47:41 -0600

    Style: [detalles Scale] se agrego stylos bold al numero interno del inventario

--- principal/extencion_agregar_detalles/inventory_insight/main.js
+++ principal/extencion_agregar_detalles/inventory_insight/main.js
@@ -1,79 +1,80 @@
-window.addEventListener('load', async () => {
-  try {
-    // ELEMENTOS INTERNOS
-    const htmlinternalLocationInv = ElementsHtml.createElement({
-      id: 'DetailPaneHeaderinternalLocationInv',
-      title: 'Internal Location Inv',
-    });
-    const htmllogisticsUnit = ElementsHtml.createElement({
-      id: 'DetailPaneHeaderlogisticsUnit',
-      title: 'Logistics Unit',
-    });
-    const htmlParentLogisticsUnit = ElementsHtml.createElement({
-      id: 'DetailPaneHeaderParentLogisticsUnit',
-      title: 'Parent Logistics Unit',
-    });
+window.addEventListener("load", async () => {
+	try {
+		// ELEMENTOS INTERNOS
+		const htmlinternalLocationInv = ElementsHtml.createElement({
+			id: "DetailPaneHeaderinternalLocationInv",
+			title: "Internal Location Inv",
+			bold: true,
+		});
+		const htmllogisticsUnit = ElementsHtml.createElement({
+			id: "DetailPaneHeaderlogisticsUnit",
+			title: "Logistics Unit",
+		});
+		const htmlParentLogisticsUnit = ElementsHtml.createElement({
+			id: "DetailPaneHeaderParentLogisticsUnit",
+			title: "Parent Logistics Unit",
+		});
 
-    // ELEMENTOS EXTERNOS
-    const htmlReceiptDateTime = ElementsHtml.createElement({
-      id: 'DetailPaneHeaderReceiptDateTime',
-      title: 'Receipt Date Time',
-    });
-    const htmlUserStamp = ElementsHtml.createElement({
-      id: 'DetailPaneHeaderUserStamp',
-      title: 'User Stamp',
-    });
-    const htmlDateTimeStamp = ElementsHtml.createElement({
-      id: 'DetailPaneHeaderDateTimeStamp',
-      title: 'Date Time Stamp',
-    });
-    const htmlAllocation = ElementsHtml.createElement({
-      id: 'DetailPaneHeaderAllocation',
-      title: 'Allocation',
-    });
-    const htmlLocating = ElementsHtml.createElement({
-      id: 'DetailPaneHeaderLocating',
-      title: 'Locating',
-    });
-    const htmlWorkZone = ElementsHtml.createElement({
-      id: 'DetailPaneHeaderWorkZone',
-      title: 'Work Zone',
-    });
-    const htmlAttribute1 = ElementsHtml.createElement({
-      id: 'DetailPaneHeaderAttribute1',
-      title: 'Attribute 1',
-    });
+		// ELEMENTOS EXTERNOS
+		const htmlReceiptDateTime = ElementsHtml.createElement({
+			id: "DetailPaneHeaderReceiptDateTime",
+			title: "Receipt Date Time",
+		});
+		const htmlUserStamp = ElementsHtml.createElement({
+			id: "DetailPaneHeaderUserStamp",
+			title: "User Stamp",
+		});
+		const htmlDateTimeStamp = ElementsHtml.createElement({
+			id: "DetailPaneHeaderDateTimeStamp",
+			title: "Date Time Stamp",
+		});
+		const htmlAllocation = ElementsHtml.createElement({
+			id: "DetailPaneHeaderAllocation",
+			title: "Allocation",
+		});
+		const htmlLocating = ElementsHtml.createElement({
+			id: "DetailPaneHeaderLocating",
+			title: "Locating",
+		});
+		const htmlWorkZone = ElementsHtml.createElement({
+			id: "DetailPaneHeaderWorkZone",
+			title: "Work Zone",
+		});
+		const htmlAttribute1 = ElementsHtml.createElement({
+			id: "DetailPaneHeaderAttribute1",
+			title: "Attribute 1",
+		});
 
-    const htmlVerMas = ElementsHtml.seeMoreInformation();
+		const htmlVerMas = ElementsHtml.seeMoreInformation();
 
-    const panelDetail = document.querySelector('#ScreenGroupColumnDetailPanelHeaderRow1Column1046');
+		const panelDetail = document.querySelector("#ScreenGroupColumnDetailPanelHeaderRow1Column1046");
 
-    const elementsHtmlToInsert = [
-      { element: htmlinternalLocationInv },
-      { element: htmllogisticsUnit },
-      { element: htmlParentLogisticsUnit },
-      { element: htmlReceiptDateTime },
-      { element: htmlUserStamp },
-      { element: htmlDateTimeStamp },
-      { element: htmlAllocation },
-      { element: htmlLocating },
-      { element: htmlWorkZone },
-      { element: htmlAttribute1 },
-      { element: htmlVerMas },
-    ];
+		const elementsHtmlToInsert = [
+			{ element: htmlinternalLocationInv },
+			{ element: htmllogisticsUnit },
+			{ element: htmlParentLogisticsUnit },
+			{ element: htmlReceiptDateTime },
+			{ element: htmlUserStamp },
+			{ element: htmlDateTimeStamp },
+			{ element: htmlAllocation },
+			{ element: htmlLocating },
+			{ element: htmlWorkZone },
+			{ element: htmlAttribute1 },
+			{ element: htmlVerMas },
+		];
 
-    const handlePanelDetail = new HandlePanelDetailInventory();
+		const handlePanelDetail = new HandlePanelDetailInventory();
 
-    const manangerPanelDetail = new ManangerPanelDetail({
-      panelDetail,
-      elementsHtmlToInsert,
-      handlePanelDetail,
-    });
+		const manangerPanelDetail = new ManangerPanelDetail({
+			panelDetail,
+			elementsHtmlToInsert,
+			handlePanelDetail,
+		});
 
-    await manangerPanelDetail.initialize();
-  } catch (error) {
-    console.error('Error al crear ManangerPanelDetail:', error);
-  }
+		await manangerPanelDetail.initialize();
+	} catch (error) {
+		console.error("Error al crear ManangerPanelDetail:", error);
+	}
 });
 
 /** Insetar en detalles
