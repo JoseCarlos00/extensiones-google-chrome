8736de1772de77172a91bd5307ed9ab4f6e9913d
JoseCarlos00 <101861240+JoseCarlos00@users.noreply.github.com>
2024-12-12 14:12:47 -0600

    Feat: [Detalles Scale]: HandlePanelDetailDataExternal: Se agregaron metodos para Obtener datos exxternos de injectando la dependencia.
    Y metodos para obtener la capacidad

--- principal/extencion_agregar_detalles/receipt/container/HandleReceiptContainer.js
+++ principal/extencion_agregar_detalles/receipt/container/HandleReceiptContainer.js
@@ -1,277 +1,240 @@
 class HandleReceiptContainer extends HandlePanelDetailDataExternal {
-  constructor() {
-    super();
+	constructor() {
+		super();
 
-    this.selectorsId = {
-      receiptId: '#DetailPaneHeaderReceiptId',
-      parent: '#DetailPaneHeaderParent',
-      receiptDate: '#DetailPaneHeaderReceiptDate',
-      checkIn: '#DetailPaneHeaderCheckIn',
-      userStamp: '#DetailPaneHeaderUserStamp',
-      trailerId: '#DetailPaneHeaderTrailerId',
-      ...this.seeMoreInformationSelector,
-    };
+		this.backgroundMessageGroup1 = "actualizar_datos_de_receipt_container_detail";
+		this.backgroundMessageGroup2 = "actualizar_datos_de_receipt_detail";
+		this.backgroundMessageGroup3 = "actualizar_datos_de_item_unit_of_measure";
+		this.headerDataExternalPrincipal = "Receipt Container Detail";
 
-    this.externalPanelElements = {
-      parent: null,
-      receiptDate: null,
-      checkIn: null,
-      userStamp: null,
-      trailerId: null,
-    };
+		this.messageMap = {
+			[this.backgroundMessageGroup1]: (datos) => this._updateContainerDetail(datos),
+			[this.backgroundMessageGroup2]: (datos) => this._updateReceiptDetail(datos),
+			[this.backgroundMessageGroup3]: (datos) => this.updateCapacityCJ(datos),
+			datos_no_encontrados: (datos) => this._handleDataNotFound(datos),
+		};
 
-    this.internalPanelElements = {
-      receiptId: null,
-    };
+		this.selectorsId = {
+			receiptId: "#DetailPaneHeaderReceiptId",
+			parent: "#DetailPaneHeaderParent",
+			receiptDate: "#DetailPaneHeaderReceiptDate",
+			checkIn: "#DetailPaneHeaderCheckIn",
+			userStamp: "#DetailPaneHeaderUserStamp",
+			internalReceiptNum: "#DetailPaneHeaderInternalReceiptNum",
+			trailerId: "#DetailPaneHeaderTrailerId",
+			...this.seeMoreInformationSelector,
+		};
 
-    this.internalPanelValue = {
-      internalReceiptNumber: null,
-      internalRecContNumber: null,
-    };
+		this.group1ExternalPanelElements = {
+			parent: null,
+			receiptDate: null,
+			checkIn: null,
+			userStamp: null,
+		};
 
-    this.panelElements = {
-      ...this.internalPanelElements,
-      ...this.externalPanelElements,
-      seeMoreInformation: null,
-    };
+		this.group2ExternalPanelElements = {
+			trailerId: null,
+		};
 
-    this.internalData = {
-      receiptId: "[aria-describedby='ListPaneDataGrid_RECEIPT_ID']",
-      internalReceiptNumber: "[aria-describedby='ListPaneDataGrid_INTERNAL_RECEIPT_NUM']",
-      internalRecContNumber: "[aria-describedby='ListPaneDataGrid_INTERNAL_REC_CONT_NUM']",
-    };
-  }
+		this.externalPanelElements = {
+			...this.group1ExternalPanelElements,
+			...this.group2ExternalPanelElements,
+		};
 
-  _initializeInternalPanelElements() {
-    return {
-      receiptId: document.querySelector(this.selectorsId.receiptId),
-    };
-  }
+		this.internalPanelElements = {
+			receiptId: null,
+			internalReceiptNum: null,
+		};
 
-  _initializeExternalPanelElements() {
-    return {
-      parent: document.querySelector(this.selectorsId.parent),
-      receiptDate: document.querySelector(this.selectorsId.receiptDate),
-      checkIn: document.querySelector(this.selectorsId.checkIn),
-      userStamp: document.querySelector(this.selectorsId.userStamp),
-      trailerId: document.querySelector(this.selectorsId.trailerId),
-    };
-  }
+		this.internalPanelValue = {
+			internalReceiptNumber: null,
+			internalRecContNumber: null,
+		};
 
-  async _cleanDetailPanel() {
-    for (const key in this.panelElements) {
-      const element = this.panelElements[key];
+		this.panelElements = {
+			...this.internalPanelElements,
+			...this.externalPanelElements,
+			seeMoreInformation: null,
+		};
 
-      if (element) {
-        element.innerHTML = '';
-        element.classList.remove('wait');
-      }
-    }
+		this.internalData = {
+			receiptId: "[aria-describedby='ListPaneDataGrid_RECEIPT_ID']",
+			internalReceiptNumber: "[aria-describedby='ListPaneDataGrid_INTERNAL_RECEIPT_NUM']",
+			internalRecContNumber: "[aria-describedby='ListPaneDataGrid_INTERNAL_REC_CONT_NUM']",
+		};
+	}
 
-    this.internalPanelValue.internalReceiptNumber = '';
-    this.internalPanelValue.internalRecContNumber = '';
-  }
+	_initializeInternalPanelElements() {
+		return {
+			receiptId: document.querySelector(this.selectorsId.receiptId),
+			internalReceiptNum: document.querySelector(this.selectorsId.internalReceiptNum),
+		};
+	}
 
-  _extraerDatosDeTr(tr) {
-    if (!tr) return;
+	_initializeExternalPanelElements() {
+		return {
+			parent: document.querySelector(this.selectorsId.parent),
+			receiptDate: document.querySelector(this.selectorsId.receiptDate),
+			checkIn: document.querySelector(this.selectorsId.checkIn),
+			userStamp: document.querySelector(this.selectorsId.userStamp),
+			trailerId: document.querySelector(this.selectorsId.trailerId),
+			capacityCJ: document.querySelector(this.selectorsId.capacityCJ),
+		};
+	}
 
-    const receiptId = this._extractAndTrim(tr.querySelector(this.internalData.receiptId));
-    const internalReceiptNumber = this._extractAndTrim(
-      tr.querySelector(this.internalData.internalReceiptNumber)
-    );
-    const internalRecContNumber = this._extractAndTrim(
-      tr.querySelector(this.internalData.internalRecContNumber)
-    );
+	async _cleanDetailPanel() {
+		super._cleanDetailPanel();
 
-    const insert = [{ element: this.panelElements.receiptId, value: receiptId }];
+		this.internalPanelValue.internalReceiptNumber = "";
+		this.internalPanelValue.internalRecContNumber = "";
+	}
 
-    // Llamar a insertarInfo con los datos extraídos
-    this._insertInfo({
-      insert,
-      internalReceiptNumber,
-      internalRecContNumber,
-    });
-  }
+	_extraerDatosDeTr(tr) {
+		if (!tr) return;
 
-  _insertInfo({ insert = [], internalReceiptNumber, internalRecContNumber }) {
-    super._insertInfo({ insert });
-    const { trailerId } = this.panelElements;
+		const receiptId = this._extractAndTrim(tr.querySelector(this.internalData.receiptId));
+		const internalReceiptNumber = this._extractAndTrim(tr.querySelector(this.internalData.internalReceiptNumber));
+		const internalRecContNumber = this._extractAndTrim(tr.querySelector(this.internalData.internalRecContNumber));
 
-    if (trailerId) {
-      trailerId.classList.remove('disabled');
-      trailerId.style.pointerEvents = 'auto';
-      trailerId.innerHTML = 'Trailer Id...';
-    }
+		const insert = [
+			{ element: this.panelElements.receiptId, value: receiptId },
+			{ element: this.panelElements.internalReceiptNum, value: internalReceiptNumber },
+		];
 
-    this.internalPanelValue.internalReceiptNumber = internalReceiptNumber;
-    this.internalPanelValue.internalRecContNumber = internalRecContNumber;
-  }
+		// Llamar a insertarInfo con los datos extraídos
+		this._insertInfo({
+			insert,
+			internalReceiptNumber,
+			internalRecContNumber,
+		});
+	}
 
-  _waitFordata(value) {
-    const text = '1346-863-28886...';
+	_insertInfo({ insert = [], internalReceiptNumber, internalRecContNumber }) {
+		// LLAMA A _cleanDetailPanel EN super._insertInfo
+		super._insertInfo({ insert });
+		this.initializeCapacityCJText();
 
-    if (value === 'trailerId') {
-      const { trailerId } = this.externalPanelElements;
-      trailerId.classList.add('wait');
-      return;
-    }
+		const { trailerId } = this.panelElements;
 
-    const sinTrailerId = Object.fromEntries(
-      Object.entries(this.externalPanelElements).filter(([key, value]) => key !== 'trailerId')
-    );
+		if (trailerId) {
+			trailerId.classList.remove("disabled");
+			trailerId.classList.remove("show-info");
+			trailerId.innerHTML = "Trailer Id...";
+		}
 
-    for (const key in sinTrailerId) {
-      const element = sinTrailerId[key];
+		this.internalPanelValue.internalReceiptNumber = internalReceiptNumber;
+		this.internalPanelValue.internalRecContNumber = internalRecContNumber;
+	}
 
-      if (element) {
-        element.innerHTML = text;
-        element.classList.add('wait');
-      }
-    }
-  }
+	_getDataFromContainerDetail() {
+		const { internalRecContNumber } = this.internalPanelValue;
 
-  _getDataFromContainerDetail() {
-    const { internalRecContNumber } = this.internalPanelValue;
+		if (internalRecContNumber) {
+			this._waitFordata(this.group1ExternalPanelElements);
 
-    if (internalRecContNumber) {
-      this._waitFordata('detail');
+			const url = `https://wms.fantasiasmiguel.com.mx/scale/details/receiptcontainer/${internalRecContNumber}?active=active`;
 
-      const url = `https://wms.fantasiasmiguel.com.mx/scale/details/receiptcontainer/${internalRecContNumber}?active=active`;
+			this._sendBackgroundMessage(url);
+		} else {
+			ToastAlert.showAlertFullTop(
+				"No se encontró la columna [Internal Container Number], por favor active la columna."
+			);
+			console.log("No se encontró el Internal Container Number");
+		}
+	}
 
-      this._sendBackgroundMessage(url);
-    } else {
-      ToastAlert.showAlertFullTop(
-        'No se encontró la columna [Internal Container Number], por favor active la columna.'
-      );
-      console.log('No se encontró el Internal Container Number');
-    }
-  }
+	_getDataFromReceiptDetail() {
+		const { internalReceiptNumber } = this.internalPanelValue;
 
-  _getDataFromReceiptDetail() {
-    const { internalReceiptNumber } = this.internalPanelValue;
+		if (internalReceiptNumber) {
+			this._waitFordata(this.group2ExternalPanelElements);
 
-    if (internalReceiptNumber) {
-      this._waitFordata('trailerId');
+			const url = `https://wms.fantasiasmiguel.com.mx/scale/details/receipt/${internalReceiptNumber}?active=active`;
 
-      const url = `https://wms.fantasiasmiguel.com.mx/scale/details/receipt/${internalReceiptNumber}?active=active`;
+			this._sendBackgroundMessage(url);
+		} else {
+			ToastAlert.showAlertFullTop("No se encontró la Columna [Internal Receipt Number], por favor active la columna.");
+			console.log("No se encontró el Internal Receipt Number");
+		}
+	}
 
-      this._sendBackgroundMessage(url);
-    } else {
-      ToastAlert.showAlertFullTop(
-        'No se encontró la Columna [Internal Receipt Number], por favor active la columna.'
-      );
-      console.log('No se encontró el Internal Receipt Number');
-    }
-  }
+	_getDataExternal() {
+		try {
+			this.setIsCancelGetDataExternal(false);
 
-  _getDataExternal(value) {
-    try {
-      this.setIsCancelGetDataExternal(false);
+			this._getDataFromContainerDetail();
+		} catch (error) {
+			console.error("Error al obtener datos externos:", error);
+		}
+	}
 
-      if (value === 'trailerId') {
-        this - this._getDataFromReceiptDetail();
-        return;
-      }
+	_setEventTrailerId() {
+		try {
+			const { trailerId } = this.panelElements;
 
-      this._getDataFromContainerDetail();
-    } catch (error) {
-      console.error('Error al obtener datos externos:', error);
-    }
-  }
+			if (trailerId) {
+				trailerId.addEventListener("click", () => {
+					trailerId.classList.add("disabled");
 
-  _setEventTrailerId() {
-    const { trailerId } = this.panelElements;
+					this.setIsCancelGetDataExternal(false);
+					this._getDataFromReceiptDetail();
+				});
+			}
+		} catch (error) {
+			console.error("Error al establecer evento de Trailer ID:", error);
+		}
+	}
 
-    if (trailerId) {
-      trailerId.addEventListener('click', () => {
-        trailerId.classList.add('disabled');
-        this._getDataExternal('trailerId');
-      });
-    }
-  }
+	_initializeDataExternal() {
+		this._listeningToBackgroundMessages();
+		this._setEventSeeMore();
+		this._setEventTrailerId();
+		this.setClickEventCapacityCJ();
+	}
 
-  _initializeDataExternal() {
-    this._listeningToBackgroundMessages();
-    this._setEventSeeMore();
-    this._setEventTrailerId();
-  }
+	_updateContainerDetail(datos) {
+		// console.log(datos);
+		const { parent, receiptDate, userStamp, checkIn } = datos;
+		// Obtener elementos del DOM
+		const elementsToUpdate = [
+			{ element: this.externalPanelElements.parent, value: parent },
+			{ element: this.externalPanelElements.receiptDate, value: receiptDate },
+			{ element: this.externalPanelElements.userStamp, value: userStamp },
+			{ element: this.externalPanelElements.checkIn, value: `Check In: ${checkIn}` },
+		];
 
-  _updateContainerDetail(datos) {
-    // console.log(datos);
-    const { parent, receiptDate, userStamp, checkIn } = datos;
-    // Obtener elementos del DOM
-    const elementsToUpdate = [
-      { element: this.externalPanelElements.parent, value: parent },
-      { element: this.externalPanelElements.receiptDate, value: receiptDate },
-      { element: this.externalPanelElements.userStamp, value: userStamp },
-      { element: this.externalPanelElements.checkIn, value: `Check In: ${checkIn}` },
-    ];
+		this._setDataExternal(elementsToUpdate);
+	}
 
-    this._setDataExternal(elementsToUpdate);
-  }
+	_updateReceiptDetail(datos) {
+		const { trailerId } = datos;
+		const { trailerId: trailerIdElement } = this.externalPanelElements;
 
-  _updateReceiptDetail(datos) {
-    const { trailerId } = datos;
-    const { trailerId: trailerIdElement } = this.externalPanelElements;
+		// Obtener elementos del DOM
+		if (trailerIdElement) {
+			trailerIdElement.innerHTML = `${trailerId}`;
+			trailerIdElement.classList.remove("wait");
+			trailerIdElement.classList.remove("disabled");
+			trailerIdElement.classList.add("show-info");
+		}
+	}
 
-    // Obtener elementos del DOM
-    if (trailerIdElement) {
-      trailerIdElement.innerHTML = `${trailerId}`;
-      trailerIdElement.classList.remove('wait');
-      trailerIdElement.classList.remove('disabled');
-      trailerIdElement.style.pointerEvents = 'none';
-    }
-  }
+	_handleDataNotFound(datos = {}) {
+		const { header } = datos;
+		if (!header) {
+			console.error("[Header] recibido de datos vacíos");
+			return;
+		}
 
-  _removeClassWait(elementsObj) {
-    const text = 'No encontrado';
-    for (const key in elementsObj) {
-      const element = elementsObj[key];
+		super._handleDataNotFound(datos);
 
-      if (element) {
-        element.innerHTML = text;
-        element.classList.remove('wait');
-      }
-    }
-  }
-
-  _handleDataNotFound(datos) {
-    const { header } = datos;
-
-    const sinTrailerId = Object.fromEntries(
-      Object.entries(this.externalPanelElements).filter(([key, value]) => key !== 'trailerId')
-    );
-
-    // Trailer Id
-    if (header === 'Receipt Detail') {
-      const { trailerId } = this.externalPanelElements;
-      trailerId.innerHTML = 'No encontrado';
-      trailerId.classList.remove('wait');
-      trailerId.classList.remove('disabled');
-      trailerIdElement.style.pointerEvents = 'none';
-    } else if (header === 'Receipt Container Detail') {
-      this._removeClassWait(sinTrailerId);
-    }
-  }
-
-  _listeningToBackgroundMessages() {
-    const messageMap = {
-      actualizar_datos_de_receipt_container_detail: datos => this._updateContainerDetail(datos),
-      actualizar_datos_de_receipt_detail: datos => this._updateReceiptDetail(datos),
-      datos_no_encontrados: datos => this._handleDataNotFound(datos),
-    };
-
-    chrome.runtime.onMessage.addListener(message => {
-      if (this.isCancelGetDataExternal) {
-        return;
-      }
-
-      if (messageMap[message.action]) {
-        const datos = message.datos;
-        messageMap[message.action](datos);
-      } else {
-        console.error('unknown background response message:', message.action);
-      }
-    });
-  }
+		// Trailer Id
+		if (header === "Receipt Detail") {
+			const { trailerId } = this.externalPanelElements;
+			trailerId.innerHTML = "No encontrado";
+			trailerId.classList.remove("wait");
+			trailerId.classList.remove("disabled");
+			trailerId.classList.add("show-info");
+		}
+	}
 }
