c80f36a7745e2d4c94b1d2b92e08384aaac63cc3
JoseCarlos00 <101861240+JoseCarlos00@users.noreply.github.com>
2025-09-03 16:23:32 -0600

    Feat: PrintScale: Create inventory insight

---
+++ principal/extencion_print_scale/inventory_insight/main.js
@@ -0,0 +1,10 @@
+// Se ejecuta cuando la página ha cargado completamente.
+window.addEventListener('load', () => {
+	// Configuración específica para esta página
+	const ulSelector = '#topNavigationBar > nav > ul.collapsepane.nav.navbar-nav';
+	const urlPrefix = 'inventory_insight/';
+
+	// Crea una instancia de la clase y la inicializa
+	const shipmentPrinter = new PrintInjector({ ulSelector, urlPrefix });
+	shipmentPrinter.init();
+});
