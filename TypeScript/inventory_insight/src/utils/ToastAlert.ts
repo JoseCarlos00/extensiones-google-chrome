a7551dfcbb5cf2c4dd9b14d7d387963212e5d4a4
JoseCarlos00 <101861240+JoseCarlos00@users.noreply.github.com>
2025-09-04 11:17:09 -0600

    Refactor: ToastAlert.ts

--- TypeScript/inventory_insight/src/utils/ToastAlert.ts
+++ TypeScript/inventory_insight/src/utils/ToastAlert.ts
@@ -3,9 +3,9 @@
  * Types: [error] [info]  [success] [warning]
  *
  * ToastAlert.showAlertFullTop('MSG', 'Type')
- * ToastAlert.showAlertMinBotton('MSG', 'Type')
+ * ToastAlert.showAlertMinButton('MSG', 'Type')
  * 
- * TODO: Inplementar todas las posiciones de alerta
+ * TODO: Implementar todas las posiciones de alerta
   .toast-top-full-width 
   .toast-bottom-full-width 
 
@@ -17,122 +17,147 @@
   .toast-top-min-width 
   .toast-bottom-min-width 
  */
+
+type ToastType = 'error' | 'info' | 'success' | 'warning';
+
+interface ToastConfig {
+  message: string;
+  type: ToastType;
+  time: number;
+  _className: string;
+}
+
 export class ToastAlert {
-  constructor({ message, type, time, _className }) {
-    this.message = message;
-    this.type = type;
-    this.className = _className;
-    this.time = time;
+	private readonly message: string;
+	private readonly type: ToastType;
+	private readonly className: string;
+	private readonly time: number;
+	private readonly container: HTMLElement | null;
 
-    this.container = document.querySelector(`.${this.className}`);
+	constructor({ message, type, time, _className }: ToastConfig) {
+		this.message = message;
+		this.type = type;
+		this.className = _className;
+		this.time = time;
 
-    // Comprobar si el contenedor existe; si no, crear uno
-    if (!this.container) {
-      this.container = document.createElement('div');
-      this.container.id = 'toast-container';
-      this.container.className = this.className;
+		this.container = document.querySelector(`.${this.className}`);
 
-      document.body.appendChild(this.container);
-    }
-  }
+		// Comprobar si el contenedor existe; si no, crear uno
+		if (!this.container) {
+			this.container = document.createElement('div');
+			this.container.id = 'toast-container';
+			this.container.className = this.className;
 
-  createToast() {
-    const toastHTML = `
+			document.body.appendChild(this.container);
+		}
+	}
+
+	private createToast() {
+		const toastHTML = /* html */ `
         <div class="toast toast-${this.type}">
           <button class="toast-close-button">×</button>
           <div class="toast-message">${this.message}</div>
         </div>
       `;
-    this.container.innerHTML += toastHTML;
 
-    // Agregar los event listeners para cerrar la alerta
-    this.addEventListeners();
-    this.setTimeDeleteAlert(this.time);
-  }
+		if (!this.container) {
+			throw new Error('No se encontró el contenedor de alerta #toast-container');
+		}
 
-  addEventListeners() {
-    const closeButton = this.container.querySelector('.toast-close-button');
-    const toastMessage = this.container.querySelector('.toast-message');
+		this.container.innerHTML += toastHTML;
 
-    if (closeButton) {
-      closeButton.addEventListener('click', () => this.hide());
-    }
+		// Agregar los event listeners para cerrar la alerta
+		this.addEventListeners();
+		this.setTimeDeleteAlert(this.time);
+	}
 
-    // Agregar el evento click al contenedor para cerrar la alerta si se hace clic fuera del toast
-    this.container.addEventListener('click', e => {
-      if (e.target === this.container || e.target === toastMessage) {
-        this.hide();
-      }
-    });
-  }
-
-  hide() {
+	private addEventListeners() {
     if (!this.container) {
-      throw new Error('No se encontro el contenedor de alerta #toast-container');
+      throw new Error('No se encontró el contenedor de alerta #toast-container');
     }
 
-    const toastElements = Array.from(this.container.querySelectorAll('.toast'));
+		const closeButton = this.container.querySelector('.toast-close-button');
+		const toastMessage = this.container.querySelector('.toast-message');
 
-    if (toastElements.length === 1) {
-      this.container.remove();
-      return;
-    }
+		if (closeButton) {
+			closeButton.addEventListener('click', () => this.hide());
+		}
 
-    toastElements.reverse().forEach((toast, index) => {
-      if (index > 0) {
-        setTimeout(() => toast.remove(), 2000);
-      } else {
-        toast.remove();
-      }
-    });
-  }
+		// Agregar el evento click al contenedor para cerrar la alerta si se hace clic fuera del toast
+		this.container.addEventListener('click', (e) => {
+			if (e.target === this.container || e.target === toastMessage) {
+				this.hide();
+			}
+		});
+	}
 
-  setTimeDeleteAlert(time) {
-    setTimeout(() => this.hide(), time);
-  }
+	private hide() {
+		if (!this.container) {
+			throw new Error('No se encontró el contenedor de alerta #toast-container');
+		}
 
-  /**
-   *
-   * @param {String} message Mensage de alerta
-   * @param {String} type Tipo de alerta: default [error]
-   *
-   */
-  static showAlertFullTop(message, type = 'error') {
-    try {
-      const configuration = {
-        message: message,
-        type: type,
-        time: 5000,
-        _className: 'toast-top-full-width',
-      };
+		const toastElements = Array.from(this.container.querySelectorAll('.toast'));
 
-      const toastAlert = new ToastAlert(configuration);
-      toastAlert.createToast();
-    } catch (error) {
-      console.error('Error: a surgido un problema al crear una alerta', error);
-    }
-  }
+		if (toastElements.length === 1) {
+			this.container.remove();
+			return;
+		}
 
-  static showAlertMinBotton(message, type = 'error') {
-    try {
-      const configuration = {
-        message: message,
-        type: type,
-        time: 3000,
-        _className: 'toast-bottom-min-width',
-      };
+		toastElements.reverse().forEach((toast, index) => {
+			if (index > 0) {
+				setTimeout(() => toast.remove(), 2000);
+			} else {
+				toast.remove();
+			}
+		});
+	}
 
-      const toastAlert = new ToastAlert(configuration);
-      toastAlert.createToast();
-    } catch (error) {
-      console.error('Error: a surgido un problema al crear una alerta', error);
-    }
-  }
+	private setTimeDeleteAlert(time: number) {
+		setTimeout(() => this.hide(), time);
+	}
+
+	/**
+	 *
+	 * @param {String} message Message de alerta
+	 * @param {String} type Tipo de alerta: default [error]
+	 *
+	 */
+	static showAlertFullTop(message: string, type: ToastType = 'error') {
+		try {
+			const configuration: ToastConfig = {
+				message: message,
+				type: type,
+				time: 5000,
+				_className: 'toast-top-full-width',
+			};
+
+			const toastAlert = new ToastAlert(configuration);
+			toastAlert.createToast();
+		} catch (error) {
+			console.error('Error: a surgido un problema al crear una alerta', error);
+		}
+	}
+
+	static showAlertMinButton(message: string, type: ToastType = 'error') {
+		try {
+			const configuration: ToastConfig = {
+				message: message,
+				type: type,
+				time: 3000,
+				_className: 'toast-bottom-min-width',
+			};
+
+			const toastAlert = new ToastAlert(configuration);
+			toastAlert.createToast();
+		} catch (error) {
+			console.error('Error: a surgido un problema al crear una alerta', error);
+		}
+	}
 }
 
 // Insertar Estilos Adicionales
 window.addEventListener('load', () => {
-  document.querySelector('head').insertAdjacentHTML(
+  document.querySelector('head')?.insertAdjacentHTML(
     'beforeend',
     `
     <style>
