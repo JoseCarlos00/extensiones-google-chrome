411f295dec421a5a6e17ae3b284425ef9ae14322
JoseCarlos00 <101861240+JoseCarlos00@users.noreply.github.com>
2025-10-14 11:54:58 -0600

    Update

--- TypeScript/modal_SQL/src/shipping_container_insight/content/adjustmentPositive.ts
+++ TypeScript/modal_SQL/src/shipping_container_insight/content/adjustmentPositive.ts
@@ -23,6 +23,9 @@ const btnCopyAjsPositive = /*html*/ `
 export const contentModalAjtPositive = /*html*/ `
   <pre class="position-relative adjustment-positive">
   ${btnCopyAjsPositive}
+  <label class="label-ajuste-positivo" style="font-weight: 600;margin-bottom: 4px; margin: 0;padding: 0;width: 470px;display: inline-block;">
+    <span style=" width: 111px; display: inline-block; ">Item</span> <span style=" width: 45px; display: inline-block; ">Qty</span> <span>Location</span>
+  </label>
   <textarea id="${idAdjustmentPositiveForm}" style="width: 470px; height: 150px; border-color: #c4c3c9; border-radius: 8px; padding: 8px;"></textarea>
   </pre>
 `;
