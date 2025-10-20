e43882e67067fc6208a0ed62d764d508c005202c
JoseCarlos00 <101861240+JoseCarlos00@users.noreply.github.com>
2025-10-20 11:49:28 -0600

    New Extension Gmail Reporte

---
+++ others/gmail_reporte/content.js
@@ -0,0 +1,121 @@
+class PedidosAutorizados {
+	constructor() {
+		this.titleE = document.querySelector('[data-subject-announcement] h2')?.textContent?.toLowerCase();
+		this.isValidTitle = this.titleE && /pedidos autorizados/.test(this.titleE);
+		this.tbodyTable = document.querySelector('[data-message-id] div div div > table tbody');
+
+		this.regexTienda = /(\D+-\D+):/;
+
+		this.pedidos = [];
+		this.initialize();
+	}
+
+	initialize() {
+		if (!this.isValidTitle) {
+			// Si el título no es el esperado, no continuamos.
+			// Podemos mostrar un mensaje si es necesario para depuración.
+			console.log('No es un correo de "Pedidos Autorizados".');
+			return false; // Indica que la inicialización falló.
+		}
+	}
+
+	setPedidos(pedidos) {
+		this.pedidos = pedidos;
+	}
+
+	getPedidos() {
+		return this.pedidos;
+	}
+
+	processingTable() {
+		if (!this.isValidTitle) {
+			return; // No procesar si el título no es válido.
+		}
+
+		if (!this.tbodyTable) {
+			console.error('No se encontró la tabla del contenido');
+			return;
+		}
+
+		// Obtiene todas las filas que contienen los spans con la información.
+		const infoRows = this.tbodyTable.querySelectorAll('tr td > span');
+		const pedidosResult = [];
+
+		infoRows.forEach((rowSpan) => {
+			// Dentro de cada span, los 'strong' contienen el nombre de la tienda y los pedidos.
+			const strongElements = Array.from(rowSpan.querySelectorAll('strong'));
+
+			// Necesitamos al menos el cliente y un pedido.
+			if (strongElements.length < 2) {
+				return;
+			}
+
+			// El primer elemento 'strong' es el cliente/tienda.
+			const tiendaElement = strongElements[0];
+			const match = tiendaElement.textContent.match(this.regexTienda);
+
+			if (match && match[1]) {
+				const tiendaName = match[1].trim()?.toLowerCase().replace(/-\s+/g, '-');
+				// console.log({tiendaName, match, Elm: tiendaElement});
+
+				// Buscar la tienda en el array 'shipments' (de tiendas.js)
+				const tiendaInfo = shipments.find((s) => s.customer === tiendaName);
+
+				if (tiendaInfo) {
+					// Usamos 'idCode'
+					const idCode = tiendaInfo.idCode;
+
+					// Los elementos restantes son los números de pedido.
+					for (let i = 1; i < strongElements.length; i++) {
+						const numeroPedido = strongElements[i].textContent.trim();
+						pedidosResult.push(`'${idCode}-${numeroPedido}'`);
+					}
+				} else {
+					console.warn(`Tienda no encontrada en tiendas.js: ${tiendaName}`);
+				}
+			}
+		});
+
+		this.setPedidos(pedidosResult);
+		console.log('Pedidos procesados:', this.getPedidos());
+	}
+
+	getSentenceSQL() {
+    const erpOrders = this.getPedidos().join('\n,');
+
+		const txt = `SELECT DISTINCT 
+  erp_order,
+  CASE 
+      WHEN erp_order IN (${erpOrders}) THEN 'Sí'
+      ELSE 'No'
+  END AS en_lista
+FROM shipment_detail
+WHERE warehouse = 'Mariano'
+AND erp_order IN (${erpOrders})
+ORDER BY 2,erp_order;`;
+
+		return txt;
+	}
+
+
+  copyToClipboard() {
+  try {
+    const text = this.getSentenceSQL();
+
+
+    if (!text) {
+      console.error('No se encontró contenido para Copiar al portapapeles');
+    }
+
+     navigator.clipboard.writeText(text).then(() => {
+				console.log('Contenido copiado al portapapeles');
+			});
+
+  } catch (err) {
+    console.error('Error al copiar al portapapeles:', err);
+  }
+}
+}
+
+const extractor = new PedidosAutorizados();
+extractor.processingTable();
---
+++ others/gmail_reporte/tiendas.js
@@ -0,0 +1,199 @@
+
+const shipments = [
+	{
+		customer: 'ags-aguascalientes',
+		idCode: '4342',
+	},
+	{
+		customer: 'bcn-carrousel',
+		idCode: '4559',
+	},
+	{
+		customer: 'bcn-mexicali',
+		idCode: '4797',
+	},
+	{
+		customer: 'bcn-tijuana',
+		idCode: '4757',
+	},
+	{
+		customer: 'coa-saltillo',
+		idCode: '4799',
+	},
+	{
+		customer: 'coa-torreon',
+		idCode: '4753',
+	},
+	{
+		customer: 'dur-durango',
+		idCode: '4756',
+	},
+	{
+		customer: 'gto-leon',
+		idCode: '3400',
+	},
+	{
+		customer: 'jal-gdl centro',
+		idCode: '359',
+	},
+	{
+		customer: 'jal-gdl palomar',
+		idCode: '4348',
+	},
+	{
+		customer: 'jal-gdl patria',
+		idCode: '4345',
+	},
+	{
+		customer: 'jal-gdl zapopan',
+		idCode: '354',
+	},
+	{
+		customer: 'me-maestros',
+		idCode: '4344',
+	},
+	{
+		customer: 'mex-adornos',
+		idCode: '444',
+	},
+	{
+		customer: 'mex-centro chica',
+		idCode: '418',
+	},
+	{
+		customer: 'mex-coacalco',
+		idCode: '361',
+	},
+	{
+		customer: 'mex-villa coapa',
+		idCode: '352',
+	},
+	{
+		customer: 'mex-coyoacan',
+		idCode: '3401',
+	},
+	{
+		customer: 'mex-flor',
+		idCode: '417',
+	},
+	{
+		customer: 'mex-centro grande',
+		idCode: '417',
+	},
+	{
+		customer: 'mex-interlomas',
+		idCode: '415',
+	},
+	{
+		customer: 'mex-lomas',
+		idCode: '350',
+	},
+	{
+		customer: 'mex-centro mylin',
+		idCode: '1171',
+	},
+	{
+		customer: 'mex-pedregal',
+		idCode: '3404',
+	},
+	{
+		customer: 'mex-polanco',
+		idCode: '356',
+	},
+	{
+		customer: 'mex-santa fe',
+		idCode: '363',
+	},
+	{
+		customer: 'mex-satelite',
+		idCode: '351',
+	},
+	{
+		customer: 'mex-tlanepantla',
+		idCode: '353',
+	},
+	{
+		customer: 'mex-del valle',
+		idCode: '360',
+	},
+	{
+		customer: 'mex-xochimilco',
+		idCode: '414',
+	},
+	{
+		customer: 'mty-centro',
+		idCode: '355',
+	},
+	{
+		customer: 'mty-citadel',
+		idCode: '3405',
+	},
+	{
+		customer: 'mty-garza sada',
+		idCode: '3406',
+	},
+	{
+		customer: 'mty-sanjeronimo',
+		idCode: '362',
+	},
+	{
+		customer: 'pue-puebla',
+		idCode: '3403',
+	},
+	{
+		customer: 'que-arboledas',
+		idCode: '4798',
+	},
+	{
+		customer: 'que-queretaro',
+		idCode: '3402',
+	},
+	{
+		customer: 'roo-cancun',
+		idCode: '4570',
+	},
+	{
+		customer: 'sin-culiacan',
+		idCode: '4755',
+	},
+	{
+		customer: 'slp-san luis potosi',
+		idCode: '3408',
+	},
+	{
+		customer: 'son-hermosillo',
+		idCode: '4574',
+	},
+	{
+		customer: 'tab-villahermosa',
+		idCode: '9955',
+	},
+	{
+		customer: 'tol-centro',
+		idCode: '3407',
+	},
+	{
+		customer: 'tol-metepec',
+		idCode: '3409',
+	},
+	{
+		customer: 'yuc-merida campestre',
+		idCode: '4346',
+	},
+	{
+		customer: 'yuc-merida',
+		idCode: '364',
+	},
+	{
+		customer: 'chi-chihuahua',
+		idCode: '9954',
+	},
+	{
+		customer: 'pue-angelopolis',
+		idCode: '9953',
+	},
+	{
+		customer: 'ver-veracruz',
+    idCode: '4573',
+	},
+];
