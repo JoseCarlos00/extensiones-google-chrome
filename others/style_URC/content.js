061fe03635e82f3fc123858e2c2e3939fcb8a839
JoseCarlos00 <101861240+JoseCarlos00@users.noreply.github.com>
2026-03-07 12:14:58 -0600

    Update

--- others/style_URC/content.js
+++ others/style_URC/content.js
@@ -1,16 +1,24 @@
 window.addEventListener('keydown', ({ key }) => {
 	if (key === 'ArrowRight') {
-		const activePageItem = document.querySelector('#paginator ul li a.active');
+		const activePageItemOld = document.querySelector('#paginator ul li a.active');
+		const activePageItem = document.querySelector('#paginator ul li.active');
 
-		if (activePageItem) {
-			const nextPageItem = activePageItem.parentElement.nextElementSibling;
+		if (activePageItemOld) {
+			const nextPageItem = activePageItemOld.parentElement.nextElementSibling;
+			nextPageItem?.querySelector('a')?.click();
+		} else if (activePageItem) {
+			const nextPageItem = activePageItem.nextElementSibling;
 			nextPageItem?.querySelector('a')?.click();
 		}
 	} else if (key === 'ArrowLeft') {
-		const activePageItem = document.querySelector('#paginator ul li a.active');
+		const activePageItemOld = document.querySelector('#paginator ul li a.active');
+		const activePageItem = document.querySelector('#paginator ul li.active');
 
-		if (activePageItem) {
-			const prevPageItem = activePageItem.parentElement.previousElementSibling;
+		if (activePageItemOld) {
+			const prevPageItem = activePageItemOld.parentElement.previousElementSibling;
+			prevPageItem?.querySelector('a')?.click();
+		} else if (activePageItem) {
+			const prevPageItem = activePageItem.previousElementSibling;
 			prevPageItem?.querySelector('a')?.click();
 		}
 	}
