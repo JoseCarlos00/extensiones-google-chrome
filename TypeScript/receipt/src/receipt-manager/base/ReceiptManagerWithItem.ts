00da76f6c9fd996fa480b557d8b54665d69c4b5d
JoseCarlos00 <101861240+JoseCarlos00@users.noreply.github.com>
2026-05-05 16:33:07 -0600

    Feat: inicio de ReceiptWithItem

--- TypeScript/receipt/src/receipt-manager/base/ReceiptManagerWithItem.ts
+++ TypeScript/receipt/src/receipt-manager/base/ReceiptManagerWithItem.ts
@@ -1,18 +1,42 @@
-import { ReceiptManagerRF } from "./ReceiptManagerRF"
+import { ReceiptManagerRF, ReceiptManagerRFConfig } from './ReceiptManagerRF';
 
-abstract class ReceiptManagerWithItem<T> extends ReceiptManagerRF<T> {
-    protected inputReceiptId: HTMLInputElement | null = null;
-    protected inputItem: HTMLInputElement | null = null;
+export abstract class ReceiptManagerWithItem<T> extends ReceiptManagerRF<T> {
+	protected inputReceiptId: HTMLInputElement | null = null;
+	protected inputItem: HTMLInputElement | null = null;
 
-    protected getContainerQty(): number {
-        const option = document.querySelector<HTMLOptionElement>('#combobox_id option');
-        const match = option?.textContent?.match(/\(([0-9,]+)\)/);
-        return match ? parseInt(match[1].replace(/,/g, '')) : 0;
-    }
+    protected tittleCurrentPage: string = '';
+	protected messageInvalideLicensePlate: string = '';
+	protected isValideLicensePlate: boolean = false;
 
-    protected calcularUnidades(openQty: number): number {
-        return Math.floor(openQty / this.getContainerQty());
-    }
+    
+	abstract nameStorageLPs: string;
 
-    protected generateLp(): string { ... } // lógica compartida de LP aleatorio+incremental
+    constructor(config: ReceiptManagerRFConfig) {
+            super(config);
+            // this.btnDone = document.querySelector<HTMLInputElement>('input[type="button"][value="Done"]');
+            // this.inputLicensePlate = this.getInput('Form1', 'CONTID');
+    
+            this.tittleCurrentPage = this.getTextByIndex('h3', 0);
+            this.messageInvalideLicensePlate = this.getTextByIndex('h3', 1);
+            this.isValideLicensePlate = this.tittleCurrentPage === 'License plate';
+        }
+
+	
+
+	protected getContainerQty(): number {
+		const option = document.querySelector<HTMLOptionElement>('#combobox_id option');
+		const match = option?.textContent?.match(/\(([0-9,]+)\)/);
+		return match ? parseInt(match[1].replace(/,/g, '')) : 0;
+	}
+
+	protected calcularUnidades(openQty: number): number {
+		return Math.floor(openQty / this.getContainerQty());
+	}
+
+	protected generateLp(): string {
+		// lógica compartida de LP aleatorio+incremental
+		const randomPart = Math.random().toString(36).substring(2, 8).toUpperCase();
+		const timestampPart = Date.now().toString(36).toUpperCase();
+		return `${randomPart}-${timestampPart}`;
+	}
 }
